bc55b2aa6483c25051866c224e03a4d1e8f72571
Gil Greenberg <ggreenberg@somethingdigital.com>
2017-01-09 09:57:50 -0500

    Include Magento 2 theme gulp file

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+// Theme Gulpfile Path
+var PATH = process.cwd() + '/vendor/somethingdigital/magento2-theme-blank';
+
+process.chdir(PATH);
+
+var gulp = require('gulp');
+var real = require(PATH + '/node_modules/gulp');
+require(PATH + '/gulpfile.js');
+
+// Steal the tasks so we can run them via argument.
+// This places them into the one in this directory, which is what "gulp" will find.
+gulp.tasks = real.tasks;
