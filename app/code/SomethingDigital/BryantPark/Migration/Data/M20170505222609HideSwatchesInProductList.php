3c6620425083d15eb9595a7e30efde01c77cc3bb
Gil Greenberg <ggreenberg@somethingdigital.com>
2017-05-05 18:31:14 -0400

    Hide swatches from product list

---
+++ app/code/SomethingDigital/BryantPark/Migration/Data/M20170505222609HideSwatchesInProductList.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SomethingDigital\BryantPark\Migration\Data;
+
+use Magento\Framework\Setup\SetupInterface;
+use SomethingDigital\Migration\Api\MigrationInterface;
+use SomethingDigital\Migration\Helper\Cms\Page as PageHelper;
+use SomethingDigital\Migration\Helper\Cms\Block as BlockHelper;
+use SomethingDigital\Migration\Helper\Email\Template as EmailHelper;
+use Magento\Config\Model\ResourceModel\Config as ResourceConfig;
+
+class M20170505222609HideSwatchesInProductList implements MigrationInterface
+{
+    protected $page;
+    protected $block;
+    protected $email;
+    protected $resourceConfig;
+
+    public function __construct(PageHelper $page, BlockHelper $block, EmailHelper $email, ResourceConfig $resourceConfig)
+    {
+        $this->page = $page;
+        $this->block = $block;
+        $this->email = $email;
+        $this->resourceConfig = $resourceConfig;
+    }
+
+    public function execute(SetupInterface $setup)
+    {
+        $this->resourceConfig->saveConfig(
+            'catalog/frontend/show_swatches_in_product_list',
+            '0',
+            'default',
+            0
+        );
+    }
+}
