49d33bf016b3ab9c1ec41617b880da454cdadf8d
Alex Borisenko <aborisenko@somethingdigital.com>
2017-05-16 18:03:16 +0300

    Increase customer session lifetime to 1 day

---
+++ app/code/SomethingDigital/CommerceAccelerator/Migration/Data/M20170516143320IncreaseSessionLifetime.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SomethingDigital\CommerceAccelerator\Migration\Data;
+
+use Magento\Framework\Setup\SetupInterface;
+use SomethingDigital\Migration\Api\MigrationInterface;
+use SomethingDigital\Migration\Helper\Cms\Page as PageHelper;
+use SomethingDigital\Migration\Helper\Cms\Block as BlockHelper;
+use SomethingDigital\Migration\Helper\Email\Template as EmailHelper;
+use Magento\Config\Model\ResourceModel\Config as ResourceConfig;
+
+class M20170516143320IncreaseSessionLifetime implements MigrationInterface
+{
+    protected $page;
+    protected $block;
+    protected $email;
+    protected $resourceConfig;
+
+    public function __construct(PageHelper $page, BlockHelper $block, EmailHelper $email, ResourceConfig $resourceConfig)
+    {
+        $this->page = $page;
+        $this->block = $block;
+        $this->email = $email;
+        $this->resourceConfig = $resourceConfig;
+    }
+
+    public function execute(SetupInterface $setup)
+    {
+        // increase customer sesion lifetime
+        $this->resourceConfig->saveConfig(
+            'web/cookie/cookie_lifetime',
+            '86400',
+            'default',
+            0
+        );
+    }
+}
