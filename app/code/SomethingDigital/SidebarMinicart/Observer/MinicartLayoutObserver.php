169fab9666cd14d3d7ca569379ac217176c37fd1
Gil Greenberg <ggreenberg@somethingdigital.com>
2017-05-22 11:44:28 -0400

    Cleanup Minicart observer code

--- app/code/SomethingDigital/SidebarMinicart/Observer/MinicartLayoutObserver.php
+++ app/code/SomethingDigital/SidebarMinicart/Observer/MinicartLayoutObserver.php
@@ -2,36 +2,30 @@
 
 namespace SomethingDigital\SidebarMinicart\Observer;
 
-
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use \Magento\Store\Model\StoreManagerInterface;
-
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class MinicartLayoutObserver implements ObserverInterface
 {
-
-    protected $_storeManager;
+    protected $_scopeConfig;
 
     /**
-     * MagezinerLoadBeforeObserver constructor.
-     * @param MagezinerBlock $magezinerBlock
+     *  MinicartLayoutObserver constructor.
+     * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
-        StoreManagerInterface $storeManager,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        ScopeConfigInterface $scopeConfig
     ) {
-        $this->_storeManager = $storeManager;
         $this->_scopeConfig = $scopeConfig;
     }
 
     public function getConfig($config_path)
     {
-        $store = $this->_storeManager->getStore()->getId();
-
         return $this->_scopeConfig->getValue(
             $config_path,
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            ScopeInterface::SCOPE_STORE
         );
     }
 
@@ -39,7 +33,7 @@ class MinicartLayoutObserver implements ObserverInterface
      * @param Observer $observer
      * @return void
      */
-    public function execute(\Magento\Framework\Event\Observer $observer)
+    public function execute(Observer $observer)
     {
         /** @var \Magento\Framework\View\Layout $layout */
         $layout = $observer->getEvent()->getData('layout');
