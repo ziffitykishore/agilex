a8607fd175c1a8ba47732c620a08431e8d709932
Dima D <d.dromov@sam-solutions.dp.ua>
2017-06-01 18:42:28 +0300

    Remove SomethingDigital/ThemeCustomisation module

--- app/code/SomethingDigital/ThemeCustomisation/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'SomethingDigital_ThemeCustomisation',
-    __DIR__
-);
