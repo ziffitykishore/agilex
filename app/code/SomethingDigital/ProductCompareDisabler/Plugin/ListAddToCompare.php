af760f8cd66f3f91941cecaf7c7ede1c14c016f4
singhchandrika <chandrikasingh16@gmail.com>
2018-01-16 15:12:37 -0500

    Plugin to disable Product Compare.

---
+++ app/code/SomethingDigital/ProductCompareDisabler/Plugin/ListAddToCompare.php
@@ -0,0 +1,11 @@
+<?php
+namespace SomethingDigital\ProductCompareDisabler\Plugin;
+class ListAddToCompare
+{
+    public function afterToHtml($subject, $result)
+    {
+        //return empty addToCompare template content
+        return '';
+    }
+}
+?>
---
+++ app/code/SomethingDigital/ProductCompareDisabler/Plugin/ViewAddToCompare.php
@@ -0,0 +1,11 @@
+<?php
+namespace SomethingDigital\ProductCompareDisabler\Plugin;
+class ViewAddToCompare
+{
+    public function afterToHtml($subject, $result)
+    {
+        //return empty addToCompare template content
+        return '';
+    }
+}
+?>
