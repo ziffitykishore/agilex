e1879bd28109aee9b6bc617ba80b1863e5a0d0a5
Nandhini Nagaraj <nandhini.nagaraj@ziffity.com>
2018-06-13 15:45:07 +0530

    career page

--- wp-content/themes/agilex/functions.php
+++ wp-content/themes/agilex/functions.php
@@ -183,7 +183,31 @@ function bootstrapcanvaswp_scripts() {
 /*     wp_enqueue_script( 'animsition-js', get_template_directory_uri() . '/js/jquery.animsition.min.js', array( 'jquery' ), '', true ); */
 
     wp_enqueue_script( 'scripts-js', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ), '', true );
-
+    $the_page = sanitize_post( $GLOBALS['wp_the_query']->get_queried_object() );
+    if ('careers' == $the_page->post_name) {
+        // Register the script
+        wp_register_script( 'career-job-js', get_template_directory_uri() .'/js/career-job.js' );
+        // Localize the script with new data
+        $translation_array =
+            array(
+                'ajaxurl'              => esc_js( admin_url('admin-ajax.php') ),
+                'setting_extensions'   => is_array( get_option('job_board_upload_file_ext') ) ? array_map( 'esc_js', get_option('job_board_upload_file_ext') ) : esc_js( get_option('job_board_upload_file_ext') ),
+                'all_extensions_check' => esc_js( get_option('job_board_all_extensions_check') ),
+                'allowed_extensions'   => is_array( get_option('job_board_allowed_extensions') ) ? array_map( 'esc_js', get_option('job_board_allowed_extensions') ) : esc_js( get_option('job_board_allowed_extensions') ),
+                'job_listing_content'  => esc_js( get_option('job_board_listing') ),
+                'jobpost_content'      => esc_js( get_option('job_board_jobpost_content') ),
+                'jquery_alerts'        => array(
+                    'invalid_extension'         => apply_filters( 'sjb_invalid_file_ext_alert', esc_html__( 'This is not an allowed file extension.', 'simple-job-board' ) ),
+                    'application_not_submitted' => apply_filters( 'sjb_job_not_submitted_alert', esc_html__('Your application could not be processed.', 'simple-job-board') ),
+                ),
+                'file' =>array(
+                    'browse' => esc_html__('Browse', 'simple-job-board'),
+                    'no_file_chosen' => esc_html__('No file chosen', 'simple-job-board'),
+                ) );
+        wp_localize_script( 'career-job-js', 'application_form', $translation_array );
+        // Enqueued script with localized data.
+        wp_enqueue_script( 'career-job-js' );
+    }
 
 }
 add_action( 'wp_enqueue_scripts', 'bootstrapcanvaswp_scripts' );
@@ -1882,7 +1906,7 @@ function highlight_results($text){
 add_filter('the_content', 'highlight_results');
 add_filter('the_excerpt', 'highlight_results');
 add_filter('the_title', 'highlight_results');
- 
+
 function highlight_results_css() {
 	?>
 	<style>
--- wp-content/themes/agilex/page-careers.php
+++ wp-content/themes/agilex/page-careers.php
@@ -33,7 +33,7 @@
 <div class="careers-wrap">
   <div class="container">
     <div class="white-bg margin-top--70 pad-70 inner-section clearfix">
-      <?php 
+      <?php
       // TO SHOW THE PAGE CONTENTS
       while ( have_posts() ) : the_post(); ?>
       <?php echo the_Content(); ?>
@@ -41,6 +41,29 @@
       endwhile; //resetting the page loop
       wp_reset_query(); //resetting the page query
       ?>
+         <?php
+
+        $postArg = array('post_type' => 'jobpost');
+        $jobPosts = get_posts( $postArg );
+	$countPosts = count($jobPosts);
+	if ($countPosts > 0) { ?>
+    <select id="job-post-list" class="job-list">
+        <?php foreach ( $jobPosts as $jobPost ) {
+                //setup_postdata($jobPost);
+                echo '<option class="post-id" value='.$jobPost->ID. '>'.$jobPost->post_title.'</option>';
+            }
+        } ?>
+    </select>
+<div id="update"><?php
+    $general_post_args = array('post_type' => 'jobpost');
+    $general_post = get_posts( $general_post_args );
+    $i = 1;
+    foreach ( $general_post as $post ):
+        setup_postdata($post);
+        echo ($i == 1) ? sjb_job_listing_application_form($post):'';
+        $i++;
+    endforeach; ?>
+ </div>
       <div class="col-sm-6  col-md-5 careers-form-wrap ">
         <div class="careers-form pad-30">
           <div class="heading text-uppercase text-center">
@@ -57,19 +80,30 @@
                 <div class="sub-heading">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
               </div>
           </div>
+            <?php
+                $post_args = array('post_type' => 'jobpost',
+                    'post_status'  => 'publish',
+                    'orderby'  => 'date',
+                    'order'   => 'DESC');
+                $job_lists = get_posts( $post_args );
+                $count_posts = count($job_lists);
+                if($count_posts>0) {
+                foreach ( $job_lists as $job_list ):
+                    setup_postdata($job_list);
+                if ($job_list->post_name != 'general-post') { ?>
+
           <div class="opening-list-sec">
             <div class="opening-list flex-sec ">
                 <div class="flex-30 flex-sm-100 post-name-wrap">
-                    <div class="post-name">Account Excutive</div>
-                    <a href="#" class="btn btn-md btn-door btn-blue">Apply Now</a>
+                    <div class="post-name"><?php echo $job_list->post_title; ?></div>
+                    <a href="#" class="btn btn-md btn-door btn-blue app-button"><?php echo __('Apply Now'); ?></a>
                 </div>
                 <div class="flex-70 flex-sm-100 job-desc">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                    </p>  
+                    <p><?php echo $job_list->post_content; ?></p>
                 </div>
             </div>
-            
-          </div>
+
+                </div><?php } endforeach; }  ?>
         </div>
       </div>
     </div>
@@ -82,7 +116,7 @@
 
       <div class="image-gallery row pad-tb-50">
         <div class="container">
-      <?php 
+      <?php
     $image_1 = get_field('image_1');
     $image_2 = get_field('image_2');
     $image_3 = get_field('image_3');
@@ -91,9 +125,9 @@
  ?>
           <div class="col-sm-6 no-pad">
           <div class="col-sm-6 img-sec wow fadeIn ">
-          
+
               <div class="img-diagonal alignnone"><img src="<?php echo $image_1['url']; ?>" alt="" class=" size-full wp-image-363" /></div>
-          
+
           </div>
           <div class="col-sm-6 img-sec wow fadeIn ">
          <?php if($image_2) {?>
@@ -120,7 +154,7 @@
           </div>
 
 
- 
+
 
 
 
