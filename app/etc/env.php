832fab5b6bffc89dad77ec22e2ca257023ec36e2
Veenamol <veena.vidhyadharan@ziffity.com>
2018-08-09 20:33:33 +0530

    homepage changes

---
+++ app/etc/env.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_dev'
+    ],
+    'crypt' => [
+        'key' => '19df38220a491780368957ef6290c4d7'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'devshopcsnnew3',
+                'username' => 'root',
+                'password' => 'admin123',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'translate' => 1,
+        'config_webservice' => 1,
+        'compiled_config' => 1
+    ],
+    'install' => [
+        'date' => 'Thu, 12 Jul 2018 07:51:15 +0000'
+    ]
+];
