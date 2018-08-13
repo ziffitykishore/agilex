106b2c75a790195ab7afab948cee97d5a0139e73
Rajasekar Senguttuvan <rajasekar@ziffity.com>
2018-08-13 11:55:26 +0530

    banner slider installed

---
+++ app/code/Mageplaza/BetterSlider/view/frontend/requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+    map: {
+         '*': {
+             'owlcarousel': 'Mageplaza_BetterSlider/js/owl.carousel.min',
+         },
+    },
+    paths: {
+         'owlcarousel': 'Mageplaza_BetterSlider/js/owl.carousel.min',
+    },
+    shim: {
+        'owlcarousel': {
+            deps: ['jquery']
+         }
+    }
+};
