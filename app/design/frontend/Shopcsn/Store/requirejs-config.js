1b8a0a4cbed8c9243fd126593635f89aa650b41f
Kishore N <kishore.nagalingam@ziffity.com>
2018-08-17 10:45:04 +0530

    list view update

--- app/design/frontend/Shopcsn/Store/requirejs-config.js
+++ app/design/frontend/Shopcsn/Store/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     paths: {
             'bminjs' : 'js/bootstrap.min',
-            'slick'  : 'js/slick.min'
+            'slick'  : 'js/slick.min',
+            'mainjs'   : 'js/main'
     },
     shim: {
         'bminjs': {
@@ -9,13 +10,16 @@ var config = {
         },
         'slick' :{
            deps : ['jquery']
-       }
+       },
+       'mainjs' :{
+            deps : ['jquery']
+        }
     },
     map: {
         '*': {
             'Magento_Checkout/js/sidebar':'js/sidebar',
             'Magento_Checkout/js/view/minicart':'js/minicart',
-            
+            'mainjs' : 'js/main'
         }
     } 
 };
