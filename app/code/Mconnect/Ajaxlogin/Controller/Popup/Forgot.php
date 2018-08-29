72908983e435c4ea5e31e39c531d4a56b250ad90
Rajasekar Senguttuvan <rajasekar@ziffity.com>
2018-08-30 01:59:47 +0530

    CSN-I140 - forgot password popup

--- app/code/Mconnect/Ajaxlogin/Controller/Popup/Forgot.php
+++ app/code/Mconnect/Ajaxlogin/Controller/Popup/Forgot.php
@@ -14,7 +14,6 @@ use Magento\Framework\App\Action\Context;
 use Magento\Framework\Escaper;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Exception\SecurityViolationException;
-use Magento\Customer\Controller\AbstractAccount;
 use Magento\Framework\Json\Helper\Data as JsonHelper;
 
 class Forgot extends Action
@@ -59,10 +58,10 @@ class Forgot extends Action
      */
     public function execute()
     {
-        $result        = array(
+        $result        = [
             'success' => false,
-            'message' => array()
-        );
+            'message' => []
+        ];
         $captchaStatus = $this->session->getResultCaptcha();
         if ($captchaStatus) {
             if (isset($captchaStatus['error'])) {
@@ -72,42 +71,38 @@ class Forgot extends Action
             }
             $result['imgSrc'] = $captchaStatus['imgSrc'];
         }
-
-
         /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
-        $email = (string)$this->getRequest()->getParam('email');
+        $email = (string) $this->getRequest()->getParam('email');
         if ($email) {
             if (!\Zend_Validate::is($email, 'EmailAddress')) {
                 $this->session->setForgottenEmail($email);
-                $result['error']     = true;
-                $result['message'][] = __('Please correct the email address.');
+                $result['error']        = true;
+                $result['message']      = __('Please correct the email address.');
             }
-
             try {
                 $this->customerAccountManagement->initiatePasswordReset(
                     $email,
                     AccountManagement::EMAIL_RESET
                 );
-                $result['success']   = true;
-                $result['message'][] = __(
+                $result['success']      = true;
+                $result['message']      = __(
                     'If there is an account associated with %1 you will receive an email with a link to reset your password.',
                     $this->escaper->escapeHtml($email)
                 );
             } catch (NoSuchEntityException $e) {
-                $result['success']   = true;
-                $result['message'][] = __(
+                $result['success']      = true;
+                $result['message']      = __(
                     'If there is an account associated with %1 you will receive an email with a link to reset your password.',
                     $this->escaper->escapeHtml($email)
                 );
                 // Do nothing, we don't want anyone to use this action to determine which email accounts are registered.
             } catch (SecurityViolationException $exception) {
-                $result['error']     = true;
-                $result['message'][] = $exception->getMessage();
+                $result['error']        = true;
+                $result['message']      = __($exception->getMessage());
             } catch (\Exception $exception) {
-                $result['error']     = true;
-                $result['message'][] = __('We\'re unable to send the password reset email.');
+                $result['error']        = true;
+                $result['message']      = __('We\'re unable to send the password reset email.');
             }
-
         }
         $this->getResponse()->setBody($this->jsonHelper->jsonEncode($result));
     }
