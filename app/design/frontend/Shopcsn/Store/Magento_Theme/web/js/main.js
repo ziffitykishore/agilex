2ddd40f01ba4547948aeececede23acb0479f319
Kishore N <kishore.nagalingam@ziffity.com>
2018-09-04 21:39:47 +0530

    layout isses, mini cart, view more button issues

--- app/design/frontend/Shopcsn/Store/Magento_Theme/web/js/main.js
+++ app/design/frontend/Shopcsn/Store/Magento_Theme/web/js/main.js
@@ -11,7 +11,8 @@ define([
     'underscore',
     'sidebar',
     'mage/translate',
-    'mage/dropdown'
+    'mage/dropdown',
+    'domReady!'
 ], function (Component, ko, _) {
     'use strict';
 
@@ -55,9 +56,55 @@ define([
         }
      });
 
+
+         // Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = jQuery('header').outerHeight();
+
+jQuery(window).scroll(function(event){
+    didScroll = true;
+    console.log('asd');
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = jQuery(window).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        jQuery('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + jQuery(window).height() < jQuery(document).height()) {
+            jQuery('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
      /* footer starts */
 
      jQuery('#sticky-social .trigger').on('click', function () {
         jQuery(this).parent().toggleClass('active');
     });
+
+
+
+
+
 });
