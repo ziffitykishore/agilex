bf13f3c9c8ba6a844e5c3438565acf0c9f737171
Sathish <sathish.subramanian@ziffity.com>
2018-09-28 11:07:06 +0530

    login & my account menu updates

---
+++ app/code/Ziffity/Core/Block/Customer/MyOrder/Link.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Ziffity\Core\Block\Customer\MyOrder;
+
+use Magento\Customer\Block\Account\SortLinkInterface;
+
+/**
+ * Class Link
+ *
+ * @api
+ * @SuppressWarnings(PHPMD.DepthOfInheritance)
+ * @since 100.0.2
+ */
+class Link extends \Magento\Framework\View\Element\Html\Link implements SortLinkInterface
+{
+    /**
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function getHref()
+    {
+        return $this->getUrl('sales/order/history');
+    }
+
+    /**
+     * {@inheritdoc}
+     * @since 100.2.0
+     */
+    public function getSortOrder()
+    {
+        return $this->getData(self::SORT_ORDER);
+    }
+}
