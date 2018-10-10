8fa05d311e803a3b56b838287880a3e03a64f7d7
veena <veena.vidhyadharan@ziffity.com>
2018-10-10 13:16:14 +0530

    task/CSN-I176-GPG code clean

--- app/code/Ziffity/Dataencryption/Controller/Index/Index.php
+++ app/code/Ziffity/Dataencryption/Controller/Index/Index.php
@@ -3,17 +3,6 @@
 namespace Ziffity\Dataencryption\Controller\Index;
 use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\App\Action\Context;
-/**
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * This is the Summary for this element.
- * 
- * @inheritDoc
- */
 class Index extends \Magento\Framework\App\Action\Action
 {
 
@@ -26,11 +15,8 @@ class Index extends \Magento\Framework\App\Action\Action
     }
 
     public function execute()
-    {
-        
+    {     
         $this->_eventManager->dispatch('ziffity_dataencryption');
-       // $resultPage->setActiveMenu('Ziffity_Blockcustomers::ziffity_blockcustomers');
-       // $resultPage->getConfig()->getTitle()->prepend(__('Block Customers'));
         return $this->resultPageFactory->create();
     }
 
