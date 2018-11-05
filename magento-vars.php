747e1d655bbde7bfca2fa743e7042afbf9d83d65
Ethan <ethan3600@gmail.com>
2018-11-05 10:12:35 -0500

    Load default store by default

--- magento-vars.php
+++ magento-vars.php
@@ -1,14 +1,42 @@
 <?php
-// enable, adjust and copy this code for each store you run
-// Store #0, default one
-//if (isHttpHost("example.com")) {
-//    $_SERVER["MAGE_RUN_CODE"] = "default";
-//    $_SERVER["MAGE_RUN_TYPE"] = "store";
-//}
-function isHttpHost($host)
+
+/**
+ * Simple check to see if the HTTP_HOST contains
+ * a specific "$domain" name
+ *
+ * Example:
+ *
+ * - https://www.example.com
+ *   - $domain = "example"
+ *   - returns: true
+ *
+ * @return bool
+ */
+function checkHost($domain)
 {
     if (!isset($_SERVER['HTTP_HOST'])) {
         return false;
     }
-    return strpos(str_replace('---', '.', $_SERVER['HTTP_HOST']), $host) === 0;
+    return strpos($_SERVER['HTTP_HOST'], $domain) !== false;
 }
+
+if (checkHost('travers') !== false) {
+    $_SERVER['MAGE_RUN_CODE'] = 'default';
+    $_SERVER['MAGE_RUN_TYPE'] = 'store';
+} else {
+    // Since we don't have the subdomain names configured
+    // let's just make sure we're loading the default store
+    // 
+    // Travers currently has two domain names:
+    // 
+    // - www.traverscanada.com
+    // - www.travers.com
+    //
+    // How we structure the subdomains is TBD
+    //
+    // @TODO We should remove this and rely on the subdomains 
+
+    $_SERVER['MAGE_RUN_CODE'] = 'default';
+    $_SERVER['MAGE_RUN_TYPE'] = 'store';
+}
+
