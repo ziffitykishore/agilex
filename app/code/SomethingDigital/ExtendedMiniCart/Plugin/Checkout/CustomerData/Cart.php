76dde5a76ce40ee1e7ee2452b7229463b90b9a24
Ethan <ethan3600@gmail.com>
2019-01-04 17:26:07 -0500

    Create setup for adding data to customer data

--- app/code/SomethingDigital/ExtendedMiniCart/Plugin/Checkout/CustomerData/Cart.php
+++ app/code/SomethingDigital/ExtendedMiniCart/Plugin/Checkout/CustomerData/Cart.php
@@ -1,12 +1,46 @@
 <?php
 
 namespace SomethingDigital\ExtendedMiniCart\Plugin\Checkout\CustomerData;
- 
-class Cart {
 
+use Magento\Quote\Model\Quote\ItemFactory;
+use Magento\Quote\Model\ResourceModel\Quote\Item;
+ 
+class Cart 
+{
+    /**
+     * @var ItemFactory
+     */
+    private $quoteItemFactory;
+
+    /** 
+     * @var Item
+     */
+    private $itemResourceModel;
+
+    public function __construct( 
+        ItemFactory $quoteItemFactory
+        Item $itemResourceModel
+    ) {
+        $this->quoteItemFactory = $quoteItemFactory;
+        $this->itemResourceModel = $itemResourceModel;
+    }
     public function afterGetSectionData(\Magento\Checkout\CustomerData\Cart $subject, array $result)
     {
-        $result['ethan_was_here'] = $result['subtotalAmount'] * 10 / 100;
+        /** @var string[][] $items */
+        $items = $result['items'];
+        foreach ($items as &$item) {
+            $item['savings'] = $this->getSavings($item);
+            $item['base_price'] = $this->getBasePrice($item);
+        }
+        $result['items'] = $items;
         return $result;
+    }
+
+    private function getSavings($item)
+    {
+    }
+
+    private function getBasePrice($item)
+    {
     }
 }
