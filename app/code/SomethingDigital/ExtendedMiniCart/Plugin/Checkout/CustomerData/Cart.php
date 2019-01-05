fa312ced47be2127d15b746043e2be6e1793040d
Ethan <ethan3600@gmail.com>
2019-01-04 20:11:12 -0500

    Format currency based on store

--- app/code/SomethingDigital/ExtendedMiniCart/Plugin/Checkout/CustomerData/Cart.php
+++ app/code/SomethingDigital/ExtendedMiniCart/Plugin/Checkout/CustomerData/Cart.php
@@ -8,6 +8,8 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Quote\Api\Data\CartItemInterface;
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Pricing\Helper\Data as PricingHelper;
+use Magento\Store\Model\StoreManagerInterface; 
  
 class Cart 
 {
@@ -26,14 +28,28 @@ class Cart
      */
     private $productRepo;
 
+    /**
+     * @var PricingHelper
+     */
+    private $pricingHelper;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
     public function __construct( 
         ItemFactory $quoteItemFactory,
         Item $itemResourceModel,
-        ProductRepositoryInterface $productRepo
+        ProductRepositoryInterface $productRepo,
+        PricingHelper $pricingHelper,
+        StoreManagerInterface $storeManager
     ) {
         $this->quoteItemFactory = $quoteItemFactory;
         $this->itemResourceModel = $itemResourceModel;
         $this->productRepo = $productRepo;
+        $this->pricingHelper = $pricingHelper;
+        $this->storeManager = $storeManager;
     }
 
     public function afterGetSectionData(\Magento\Checkout\CustomerData\Cart $subject, array $result)
@@ -48,11 +64,16 @@ class Cart
                 $product = $this->productRepo->get($cartItem->getSku());
             } catch (NoSuchEntityException $e) {
                 continue;
-                // @TODO
             }
             if ($cartItem->getPrice() !== $product->getPrice()) {
                 $item['savings'] = $this->getSavings($cartItem, $product);
                 $item['base_price'] = $product->getPrice();
+                $item['base_price_fmt'] = $this->pricingHelper->currencyByStore(
+                    $product->getPrice(),
+                    $this->storeManager->getStore(),
+                    true,
+                    false
+                );
                 $item['special_price'] = $product->getSpecialPrice() ?? false;
                 $item['manufacturer_price'] = $product->getManufacturerPrice() ?? false;
             }
@@ -65,7 +86,8 @@ class Cart
     {
         /** @var float $basePrice */
         $basePrice = $product->getPrice();
-        $msrpPrice = $product->getManufacturerPrice();
+        /** @var float|null $basePrice */
+        $msrpPrice = $product->getManufacturerPrice() ?? $basePrice;
         if ($basePrice > $msrpPrice) {
             $basePrice = $msrpPrice;
         }
