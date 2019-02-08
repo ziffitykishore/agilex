52d3c5545394b6b08c1d73c7b38ad637eb5d81d1
Kyle Leighton <kyle.leighton1@gmail.com>
2019-02-07 20:49:25 -0500

    Prevent about/contact links from being added to every nav ul

--- app/design/frontend/Travers/default/js/global/theme.js
+++ app/design/frontend/Travers/default/js/global/theme.js
@@ -6,8 +6,8 @@ domready(() => {
     $('.panel.header > .header.links').clone().appendTo('#store\\.links');
 
     // this clones the custom header links from desktop to the mobile main menu
-    $('.contact-link').first().clone().appendTo('.navigation ul');
-    $('.about-link').first().clone().appendTo('.navigation ul');
+    $('.contact-link').first().clone().appendTo('.navigation ul:first');
+    $('.about-link').first().clone().appendTo('.navigation ul:first');
     $('.phone-number').first().clone().prependTo('.page-header');
     $('#algolia-searchbox').append('<button type="submit" class="search-button">Search</button>');
 });
