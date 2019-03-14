f6b7c19d4ffb69bb084fc3a79152aa0d4508eea5
Adrian <asobkowski@somethingdigital.com>
2019-03-14 18:33:31 +0000

    changes after PR review #77

--- app/code/SomethingDigital/CategoryAttributes/Controller/Grouping/View.php
+++ app/code/SomethingDigital/CategoryAttributes/Controller/Grouping/View.php
@@ -49,35 +49,39 @@ class View extends \Magento\Framework\App\Action\Action
     public function execute() 
     {       
         $cid = $this->getRequest()->getParam('id');
-        $category = $this->categoryRepository->get($cid, $this->storeManager->getStore()->getId());
+        try {
+            $category = $this->categoryRepository->get($cid, $this->storeManager->getStore()->getId());
+        
+            $attrArray = array();
+            $data = array();
+            $attrArray []= $category->getGroupingAttribute1();
+            $attrArray []= $category->getGroupingAttribute2();
+            $attrArray []= $category->getGroupingAttribute3();
 
-        $attrArray = array();
-        $data = array();
-        $attrArray []= $category->getGroupingAttribute1();
-        $attrArray []= $category->getGroupingAttribute2();
-        $attrArray []= $category->getGroupingAttribute3();
-
-        foreach ($attrArray as $key => $attrCode) {
-            if (!empty($attrCode)) {
-                $attrOptions = $this->productAttributeRepository->get($attrCode)->getOptions();
-                foreach ($attrOptions as $option) {
-                    if ($option->getLabel() != " ") {
-                        $optionCode = strtolower(str_replace(' ', '-', $option->getLabel())); 
-                        $optionExists = true;
-                        try {
-                            $optionHtml = $this->blockRepository->getById('grouping_' . $attrCode . '_' . $optionCode);
-                        } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
-                            $optionExists = false;
-                        }
-                        if ($optionExists) {
-                            $filteredOptionHtml = $this->filterProvider->getPageFilter()->filter($optionHtml->getContent());
-                            $data[$attrCode][] = array($optionCode => $filteredOptionHtml);
+            foreach ($attrArray as $key => $attrCode) {
+                if (!empty($attrCode)) {
+                    $attrOptions = $this->productAttributeRepository->get($attrCode)->getOptions();
+                    foreach ($attrOptions as $option) {
+                        if (trim($option->getLabel()) != '') {
+                            $optionCode = strtolower(str_replace(' ', '-', $option->getLabel())); 
+                            $optionExists = true;
+                            try {
+                                $optionHtml = $this->blockRepository->getById('grouping_' . $attrCode . '_' . $optionCode);
+                            } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
+                                $optionExists = false;
+                            }
+                            if ($optionExists) {
+                                $filteredOptionHtml = $this->filterProvider->getPageFilter()->filter($optionHtml->getContent());
+                                $data[$attrCode][$optionCode] = $filteredOptionHtml;
+                            }
                         }
                     }
                 }
-            }
-        }        
+            }        
 
-        $this->getResponse()->representJson($this->jsonEncoder->encode($data))->setHeader('Cache-Control', 'max-age=86400, public');
+            $this->getResponse()->representJson($this->jsonEncoder->encode($data))->setHeader('Cache-Control', 'max-age=86400, public');
+        } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
+            throw new \Magento\Framework\Exception\NotFoundException(__('Category does not exists.')); 
+        }
     }
 }
