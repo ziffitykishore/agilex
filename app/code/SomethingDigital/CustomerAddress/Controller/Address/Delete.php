e34009b1e5ec39dd25d60c4e9cbdb176eebfd2ae
Adrian <asobkowski@somethingdigital.com>
2019-03-26 18:29:45 +0000

    preventing editing and deleting addresses if is_read_only is 1

---
+++ app/code/SomethingDigital/CustomerAddress/Controller/Address/Delete.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SomethingDigital\CustomerAddress\Controller\Address;
+ 
+class Delete extends \Magento\Customer\Controller\Address\Delete
+{
+ 
+	public function execute()
+	{
+        $addressId = $this->getRequest()->getParam('id', false);
+
+        if ($addressId && $this->_formKeyValidator->validate($this->getRequest())) {
+            try {
+                $address = $this->_addressRepository->getById($addressId);
+                $isReadOnly = $address->getCustomAttribute('is_read_only');
+                if ($isReadOnly->getValue()) {
+                	return $this->resultRedirectFactory->create()->setPath('*/*/index');
+                }
+                if ($address->getCustomerId() === $this->_getSession()->getCustomerId()) {
+                    $this->_addressRepository->deleteById($addressId);
+                    $this->messageManager->addSuccess(__('You deleted the address.'));
+                } else {
+                    $this->messageManager->addError(__('We can\'t delete the address right now.'));
+                }
+            } catch (\Exception $other) {
+                $this->messageManager->addException($other, __('We can\'t delete the address right now.'));
+            }
+        }
+        return $this->resultRedirectFactory->create()->setPath('*/*/index');
+	}
+ 
+}
---
+++ app/code/SomethingDigital/CustomerAddress/Controller/Address/Edit.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SomethingDigital\CustomerAddress\Controller\Address;
+
+class Edit extends \Magento\Customer\Controller\Address\Edit
+{
+    /**
+     * Customer address edit action
+     *
+     * @return \Magento\Framework\Controller\Result\Forward
+     */
+    public function execute()
+    {
+        $addressId = $this->getRequest()->getParam('id', false);
+        $address = $this->_addressRepository->getById($addressId);
+        $isReadOnly = $address->getCustomAttribute('is_read_only');
+        if ($isReadOnly->getValue()) {
+            return $this->resultRedirectFactory->create()->setPath('*/*/index');
+        }
+        /** @var \Magento\Framework\Controller\Result\Forward $resultForward */
+        $resultForward = $this->resultForwardFactory->create();
+        return $resultForward->forward('form');
+    }
+}
