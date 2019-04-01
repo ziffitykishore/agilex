a91ff01a811814381c381769e5e566adca9843da
Adrian <asobkowski@somethingdigital.com>
2019-04-01 18:46:57 +0000

    Update requirejs-config file

--- app/code/SomethingDigital/CheckoutAddress/view/frontend/requirejs-config.js
+++ app/code/SomethingDigital/CheckoutAddress/view/frontend/requirejs-config.js
@@ -4,7 +4,15 @@ var config = {
             'Magento_Checkout/template/shipping-address/list.html':
                 'SomethingDigital_CheckoutAddress/template/shipping-address/list.html',
             'Magento_NegotiableQuote/template/shipping-address/list.html':
-                'SomethingDigital_CheckoutAddress/template/shipping-address/list.html'
+                'SomethingDigital_CheckoutAddress/template/shipping-address/list.html',
+            'Magento_Checkout/template/billing-address.html':
+                'SomethingDigital_CheckoutAddress/template/billing-address.html',
+            'Magento_Checkout/js/view/billing-address':
+                'SomethingDigital_CheckoutAddress/js/view/billing-address',
+            'Magento_Checkout/js/model/shipping-save-processor/default':
+                'SomethingDigital_CheckoutAddress/js/model/shipping-save-processor/default',
+            'Magento_Checkout/js/model/checkout-data-resolver':
+                'SomethingDigital_CheckoutAddress/js/model/checkout-data-resolver'
         }
     },
     config: {
