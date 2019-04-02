db512e7202623870ff493bba352de3fdb831d0a3
Adrian <asobkowski@somethingdigital.com>
2019-04-02 18:36:12 +0000

    Replace options id with label

--- app/code/SomethingDigital/Inventory/Plugin/Catalog/Model/ProductPlugin.php
+++ app/code/SomethingDigital/Inventory/Plugin/Catalog/Model/ProductPlugin.php
@@ -7,20 +7,20 @@ class ProductPlugin
     public function beforeBeforeSave(\Magento\Catalog\Model\Product $subject)
     {
         $stockData = [];
-        if ($subject->getSxInventoryStatus() == 1) {
+        if ($subject->getAttributeText('sx_inventory_status') == 'DNR') {
             $stockData = [
                 'backorders' => 0,
                 'use_config_manage_stock' => 0,
                 'manage_stock' => 1
             ];
-        } elseif ($subject->getSxInventoryStatus() == 2) {
+        } elseif ($subject->getAttributeText('sx_inventory_status') == 'Order as needed') {
             $stockData = [
                 'backorders' => 0,
                 'use_config_manage_stock' => 0,
                 'is_in_stock' => 1,
                 'manage_stock' => 0
             ];
-        } elseif ($subject->getSxInventoryStatus() == 3) {
+        } elseif ($subject->getAttributeText('sx_inventory_status') == 'Stock') {
             $stockData = [
                 'backorders' => 2,
                 'use_config_manage_stock' => 0,
