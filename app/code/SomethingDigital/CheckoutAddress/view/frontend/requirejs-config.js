43b714c20ba12bb4f2acf364eb735e80a0dcf76a
Adrian <asobkowski@somethingdigital.com>
2019-04-03 19:52:04 +0000

    Remove overrides

--- app/code/SomethingDigital/CheckoutAddress/view/frontend/requirejs-config.js
+++ app/code/SomethingDigital/CheckoutAddress/view/frontend/requirejs-config.js
@@ -5,14 +5,8 @@ var config = {
                 'SomethingDigital_CheckoutAddress/template/shipping-address/list.html',
             'Magento_NegotiableQuote/template/shipping-address/list.html':
                 'SomethingDigital_CheckoutAddress/template/shipping-address/list.html',
-            'Magento_Checkout/template/billing-address.html':
-                'SomethingDigital_CheckoutAddress/template/billing-address.html',
-            'Magento_Checkout/js/view/billing-address':
-                'SomethingDigital_CheckoutAddress/js/view/billing-address',
             'Magento_Checkout/js/model/shipping-save-processor/default':
-                'SomethingDigital_CheckoutAddress/js/model/shipping-save-processor/default',
-            'Magento_Checkout/js/model/checkout-data-resolver':
-                'SomethingDigital_CheckoutAddress/js/model/checkout-data-resolver'
+                'SomethingDigital_CheckoutAddress/js/model/shipping-save-processor/default'
         }
     },
     config: {
@@ -22,7 +16,7 @@ var config = {
             },
             'Magento_Checkout/js/view/shipping-address/address-renderer/default': {
                 'SomethingDigital_CheckoutAddress/js/view/shipping-address/address-renderer/default-mixin': true
-            },
+            }
         }
     }
 };
