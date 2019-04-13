972d46c7eec88696c868f45ddb65fe4e6f81eb41
Todd Christensen <tchristensen@somethingdigital.com>
2019-04-12 19:14:30 -0700

    Add default settings and websites to config.php.
    We're already committing it, this allows setup:di:compile on Accelerator too.
    
    If this commit conflicts on a merge, just skip these changes.

--- app/etc/config.php
+++ app/etc/config.php
@@ -300,5 +300,110 @@ return [
         'Unirgy_SimpleUp' => 1,
         'Unirgy_SimpleLicense' => 1,
         'Vertex_Tax' => 1
+    ],
+    'scopes' => [
+        'websites' => [
+            'admin' => [
+                'website_id' => '0',
+                'code' => 'admin',
+                'name' => 'Admin',
+                'sort_order' => '0',
+                'default_group_id' => '0',
+                'is_default' => '0'
+            ],
+            'base' => [
+                'website_id' => '1',
+                'code' => 'base',
+                'name' => 'Main Website',
+                'sort_order' => '0',
+                'default_group_id' => '1',
+                'is_default' => '1'
+            ]
+        ],
+        'groups' => [
+            [
+                'group_id' => '0',
+                'website_id' => '0',
+                'code' => 'default',
+                'name' => 'Default',
+                'root_category_id' => '0',
+                'default_store_id' => '0'
+            ],
+            [
+                'group_id' => '1',
+                'website_id' => '1',
+                'code' => 'main_website_store',
+                'name' => 'General Store',
+                'root_category_id' => '2',
+                'default_store_id' => '1'
+            ]
+        ],
+        'stores' => [
+            'admin' => [
+                'store_id' => '0',
+                'code' => 'admin',
+                'website_id' => '0',
+                'group_id' => '0',
+                'name' => 'Admin',
+                'sort_order' => '0',
+                'is_active' => '1'
+            ],
+            'default' => [
+                'store_id' => '1',
+                'code' => 'default',
+                'website_id' => '1',
+                'group_id' => '1',
+                'name' => 'Default Store View',
+                'sort_order' => '0',
+                'is_active' => '1'
+            ]
+        ]
+    ],
+    'system' => [
+        'default' => [
+            'general' => [
+                'locale' => [
+                    'code' => 'en_US'
+                ]
+            ],
+            'dev' => [
+                'static' => [
+                    'sign' => '1'
+                ],
+                'front_end_development_workflow' => [
+                    'type' => 'server_side_compilation'
+                ],
+                'template' => [
+                    'minify_html' => '0'
+                ],
+                'js' => [
+                    'merge_files' => '0',
+                    'minify_files' => '1',
+                    'minify_exclude' => '
+                                  /tiny_mce/
+                              ',
+                    'session_storage_logging' => '0',
+                    'translate_strategy' => 'dictionary'
+                ],
+                'css' => [
+                    'minify_files' => '1',
+                    'minify_exclude' => '
+                                  /tiny_mce/
+                              '
+                ]
+            ],
+            'web' => [
+                'seo' => [
+                    'use_rewrites' => '1'
+                ]
+            ]
+        ],
+    ],
+    'admin_user' => [
+        'locale' => [
+            'code' => [
+                'en_US'
+            ]
+        ]
     ]
 ];
