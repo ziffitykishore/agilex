bb0eba81c51810de568408a9b9aeeef8ceb99730
Natalia Shabaieva <natalia.shdnepr@gmail.com>
2019-04-19 22:56:13 +0300

    Add storefront widget to enable slick

---
+++ app/code/SomethingDigital/PageBuilderExtensionProducts/view/base/web/js/content-type/products/appearance/grid/widget.js
@@ -0,0 +1,42 @@
+define([
+    'jquery',
+    'slick',
+    'SomethingDigital_PageBuilderExtensionProducts/js/breakpoints'
+], function ($, slick, breakpoints) {
+    'use strict';
+
+    return function (config, element) {
+        var $element = $(element);
+        var $products = $(element).find('.product-items');
+
+        if($element.data('isSlider')) {
+            if ($products.hasClass('slick-initialized')) {
+                $products.slick('unslick');
+            }
+
+            $products.slick({
+                autoplay: $element.data('productsAutoplay'),
+                autoplaySpeed: $element.data('productsAutoplaySpeed') || 0,
+                slidesToShow: $element.data('slidesToShow'),
+                fade: $element.data('productsFade'),
+                infinite: $element.data('productsInfiniteLoop'),
+                arrows: $element.data('productsShowArrows'),
+                dots: $element.data('productsShowDots'),
+                responsive: [
+                    {
+                        breakpoint: breakpoints.screen__m,
+                        settings: {
+                            slidesToShow: $element.data('slidesToShowTablet'),
+                        }
+                    },
+                    {
+                        breakpoint: breakpoints.screen__xs,
+                        settings: {
+                            slidesToShow: $element.data('slidesToShowMobile'),
+                        }
+                    }
+                ]
+            });
+        }
+    };
+});
