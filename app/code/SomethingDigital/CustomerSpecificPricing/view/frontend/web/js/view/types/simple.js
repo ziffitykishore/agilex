db32fb1ef6ffa24720ab9240acf4e84da6fbfb85
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-05-01 16:15:13 -0400

    Update only final price

--- app/code/SomethingDigital/CustomerSpecificPricing/view/frontend/web/js/view/types/simple.js
+++ app/code/SomethingDigital/CustomerSpecificPricing/view/frontend/web/js/view/types/simple.js
@@ -5,7 +5,7 @@ define([
         var sku = $('[itemprop=sku]').text();
         var price = data[sku];
         if (price != null && price != 0) {
-            $('.product-info-main .price').text('$' + price);
+            $('.product-info-main div.price-final_price > span:not(.old-price) .price').text('$' + price);
         }
     }
 });
