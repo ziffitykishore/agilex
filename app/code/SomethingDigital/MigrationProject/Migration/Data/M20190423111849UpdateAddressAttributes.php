980c9a31de29100ca415660f95f6046b0f82d454
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-05-03 09:50:16 -0400

    Clean up migration

--- app/code/SomethingDigital/MigrationProject/Migration/Data/M20190423111849UpdateAddressAttributes.php
+++ app/code/SomethingDigital/MigrationProject/Migration/Data/M20190423111849UpdateAddressAttributes.php
@@ -8,8 +8,7 @@ use SomethingDigital\Migration\Helper\Cms\Page as PageHelper;
 use SomethingDigital\Migration\Helper\Cms\Block as BlockHelper;
 use SomethingDigital\Migration\Helper\Email\Template as EmailHelper;
 use Magento\Config\Model\ResourceModel\Config as ResourceConfig;
-use Magento\Customer\Setup\CustomerSetupFactory;
-use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+use Magento\Eav\Setup\EavSetupFactory;
 
 class M20190423111849UpdateAddressAttributes implements MigrationInterface
 {
@@ -17,15 +16,14 @@ class M20190423111849UpdateAddressAttributes implements MigrationInterface
     protected $block;
     protected $email;
     protected $resourceConfig;
+    protected $eavSetupFactory;
 
-    public function __construct(PageHelper $page, BlockHelper $block, EmailHelper $email, ResourceConfig $resourceConfig, CustomerSetupFactory $customerSetupFactory, AttributeSetFactory $attributeSetFactory,\Magento\Eav\Setup\EavSetupFactory $eavSetupFactory)
+    public function __construct(PageHelper $page, BlockHelper $block, EmailHelper $email, ResourceConfig $resourceConfig, EavSetupFactory $eavSetupFactory)
     {
         $this->page = $page;
         $this->block = $block;
         $this->email = $email;
         $this->resourceConfig = $resourceConfig;
-        $this->customerSetupFactory = $customerSetupFactory;
-        $this->attributeSetFactory = $attributeSetFactory;
         $this->eavSetupFactory = $eavSetupFactory;
     }
 
