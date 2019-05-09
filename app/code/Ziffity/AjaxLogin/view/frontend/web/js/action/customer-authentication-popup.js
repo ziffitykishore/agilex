21bb5390cc75b6d7c09c71cec3dddbecf418315b
ajith <ajith.kumar@ziffity.com>
2019-05-09 17:07:59 +0530

    GC-34 Ajax Login and Signup Added

---
+++ app/code/Ziffity/AjaxLogin/view/frontend/web/js/action/customer-authentication-popup.js
@@ -0,0 +1,200 @@
+define([
+    'jquery',
+    'Magento_Ui/js/modal/modal',
+    'mage/storage',
+    'mage/translate',
+    'jquery/ui'
+], function ($, modal,storage, $t) {
+    'use strict';
+
+    $.widget('ajaxlogin.customerAuthenticationPopup', {
+        options: {
+            login: '#customer-popup-login',
+            loginLink: '#customer-popup-sign-in',
+            register: '#customer-popup-register',
+            registerLink: '#customer-popup-registration',
+            forgot : '#customer-popup-forgot',
+            forgotLink: '#customer-forgot-popup'
+        },
+
+        /**
+         *
+         * @private
+         */
+        _create: function () {
+            var self = this,
+                authentication_options = {
+                    type: 'popup',
+                    clickableOverlay: false,
+                    modalClass: 'popup-modal',
+                    responsive: true,
+                    innerScroll: true,
+                    title: this.options.popupTitle,
+                    buttons: false
+                };
+
+            modal(authentication_options, this.element);
+
+            $('body').on('click', '.signin-link, '+self.options.loginLink, function() {
+                $(self.options.register).modal('closeModal');
+                $(self.options.forgot).modal('closeModal');
+                $(self.options.login).modal('openModal');
+                self._setStyleCss();
+                return false;
+            });
+
+            $('body').on('click', self.options.registerLink, function() {
+                $(self.options.login).modal('closeModal');
+                $(self.options.register).modal('openModal');
+                self._setStyleCss(self.options.innerWidth);
+                return false;
+            });
+            
+            $('body').on('click', "a.forget-link," + self.options.forgotLink, function() {
+                $(self.options.login).modal('closeModal');
+                $(self.options.register).modal('closeModal');
+                $(self.options.forgot).modal('openModal');
+                self._setStyleCss(self.options.innerWidth);
+                return false;
+            });
+            
+            $('.popup-modal').on('modalopened', function () {
+                $('.popup-modal .mage-error').each(function() {
+                    if ($(this).is('div')) {
+                         $(this).remove();
+                    }else{
+                        $(this).removeClass('mage-error');
+                    }
+                });
+            });
+            this._ajaxSubmit();
+            this._resetStyleCss();
+        },
+
+        /**
+         * Set width of the popup
+         * @private
+         */
+        _setStyleCss: function(width) {
+            width = width || 400;
+            if (window.innerWidth > 786) {
+                this.element.parent().parent('.modal-inner-wrap').css({'max-width': width+'px'});
+            }
+        },
+
+        /**
+         * Reset width of the popup
+         * @private
+         */
+        _resetStyleCss: function() {
+            var self = this;
+            $( window ).resize(function() {
+                if (window.innerWidth <= 786) {
+                    self.element.parent().parent('.modal-inner-wrap').css({'max-width': 'initial'});
+                } else {
+                    self._setStyleCss(self.options.innerWidth);
+                }
+            });
+        },
+
+        /**
+         * Submit data by Ajax
+         * @private
+         */
+        _ajaxSubmit: function() {
+            var self = this,
+                form = this.element.find('form'),
+                inputElement = form.find('input');
+
+            inputElement.keyup(function (e) {
+                self.element.find('.messages').html('');
+            });
+
+            form.submit(function (e) {
+                if (form.validation('isValid')) {
+                    if (form.hasClass('form-create-account') || form.hasClass('form-forgot')) {
+                        $.ajax({
+                            url: $(e.target).attr('action'),
+                            data: $(e.target).serializeArray(),
+                            showLoader: true,
+                            type: 'POST',
+                            dataType: 'json',
+                            success: function (response) {
+                                self._showResponse(response, form.find('input[name="redirect_url"]').val());
+                            },
+                            error: function() {
+                                self._showFailingMessage();
+                            }
+                        });
+                    } else {
+                        var submitData = {},
+                        formDataArray = $(e.target).serializeArray();
+                        formDataArray.forEach(function (entry) {
+                            submitData[entry.name] = entry.value;
+                        });
+                        $('body').loader().loader('show');
+                        storage.post(
+                            $(e.target).attr('action'),
+                            JSON.stringify(submitData)
+                        ).done(function (response) {
+                            $('body').loader().loader('hide');
+                            self._showResponse(response, form.find('input[name="redirect_url"]').val());
+                        }).fail(function () {
+                            $('body').loader().loader('hide');
+                            self._showFailingMessage();
+                        });
+                    }
+                }
+                return false;
+            });
+        },
+
+        /**
+         * Display messages on the screen
+         * @private
+         */
+        _displayMessages: function(className, message) {
+            $('<div class="message '+className+'"><div>'+message+'</div></div>').appendTo(this.element.find('.messages'));
+        },
+
+        /**
+         * Showing response results
+         * @private
+         * @param {Object} response
+         * @param {String} locationHref
+         */
+        _showResponse: function(response, locationHref) {
+            var self = this,
+                timeout = 800;
+            this.element.find('.messages').html('');
+            if (response.errors || response.error) {
+                this._displayMessages('message-error error', response.message);
+            } else {
+                this._displayMessages('message-success success', response.message);
+            }
+            this.element.find('.messages .message').show();
+            $('#customer-popup-register').scrollTop(0);
+                        console.log('tur');
+            if(typeof locationHref !== 'undefined') {
+                 setTimeout(function() {
+                if (!response.errors) {
+                    self.element.modal('closeModal');
+                    window.location.href = locationHref;
+                }
+            }, timeout);
+            }
+        },
+
+        /**
+         * Show the failing message
+         * @private
+         */
+        _showFailingMessage: function() {
+            this.element.find('.messages').html('');
+            this._displayMessages('message-error error', $t('An error occurred, please try again later.'));
+            this.element.find('.messages .message').show();
+        }
+    });
+
+    return $.ajaxlogin.customerAuthenticationPopup;
+});
