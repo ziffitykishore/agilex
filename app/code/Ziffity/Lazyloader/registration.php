8abd592262dbcafc2f239685e6c248e33be96d5a
ajith <ajith.kumar@ziffity.com>
2019-05-10 11:54:10 +0530

    Lazyloader implemented

---
+++ app/code/Ziffity/Lazyloader/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ziffity_Lazyloader',
+    __DIR__
+);
---
+++ app/code/Ziffity/Lazyloader/view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+    map: {
+        '*': {
+            infinitescroll: 'Ziffity_Lazyloader/js/infinitescroll'
+        }
+    },
+    paths: {
+        jqueryIas: 'Ziffity_Lazyloader/js/jquery-ias.min'
+    },
+    shim : {
+        jqueryIas: ['jquery']
+    }
+};
