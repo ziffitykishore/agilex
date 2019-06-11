0805fa3849c0b2ff5459baac9e3e39e3a2e4c74e
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-06-11 15:46:41 +0200

    Remove bluefoot from migration

--- app/code/SomethingDigital/MigrationProject/Migration/Data/M20181218143743CreateSampleCatalogPage2.php
+++ app/code/SomethingDigital/MigrationProject/Migration/Data/M20181218143743CreateSampleCatalogPage2.php
@@ -6,7 +6,6 @@ use Magento\Framework\Setup\SetupInterface;
 use SomethingDigital\Migration\Api\MigrationInterface;
 use SomethingDigital\Migration\Helper\Cms\Page as PageHelper;
 use SomethingDigital\Migration\Helper\Cms\Block as BlockHelper;
-use SomethingDigital\Migration\Helper\Cms\Bluefoot as BluefootHelper;
 use SomethingDigital\Migration\Helper\Email\Template as EmailHelper;
 use Magento\Config\Model\ResourceModel\Config as ResourceConfig;
 
@@ -18,11 +17,10 @@ class M20181218143743CreateSampleCatalogPage2 implements MigrationInterface
     protected $email;
     protected $resourceConfig;
 
-    public function __construct(PageHelper $page, BlockHelper $block, BluefootHelper $bluefoot, EmailHelper $email, ResourceConfig $resourceConfig)
+    public function __construct(PageHelper $page, BlockHelper $block, EmailHelper $email, ResourceConfig $resourceConfig)
     {
         $this->page = $page;
         $this->block = $block;
-        $this->bluefoot = $bluefoot;
         $this->email = $email;
         $this->resourceConfig = $resourceConfig;
     }
