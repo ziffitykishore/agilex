619fd68b85d836dd5a82492003c2488028021df2
ajith <ajith.kumar@ziffity.com>
2019-06-21 15:13:20 +0530

    PDP layout customization

---
+++ app/code/Ziffity/Bundle/Block/Catalog/Product/View/Type/Bundle/Option/Checkbox.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ziffity\Bundle\Block\Catalog\Product\View\Type\Bundle\Option;
+
+class Checkbox extends \Magento\Bundle\Block\Catalog\Product\View\Type\Bundle\Option
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'Ziffity_Bundle::catalog/product/view/type/bundle/option/checkbox.phtml';
+}
---
+++ app/code/Ziffity/Bundle/Block/Catalog/Product/View/Type/Bundle/Option/Select.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ziffity\Bundle\Block\Catalog\Product\View\Type\Bundle\Option;
+
+class Select extends \Magento\Bundle\Block\Catalog\Product\View\Type\Bundle\Option
+{
+
+    protected $_template = 'Ziffity_Bundle::catalog/product/view/type/bundle/option/select.phtml';
+}
