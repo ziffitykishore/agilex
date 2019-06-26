165f1c1b72d218428ff27db6339d62fe00082840
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-06-26 18:45:50 +0200

    Add comment explaining purpose of the observer

--- app/code/SomethingDigital/CartRulesCustomizations/Observer/ModifyGiftQtyAfterMerge.php
+++ app/code/SomethingDigital/CartRulesCustomizations/Observer/ModifyGiftQtyAfterMerge.php
@@ -8,6 +8,8 @@ use Magento\Framework\Event\ObserverInterface;
 class ModifyGiftQtyAfterMerge implements ObserverInterface
 {
     /**
+     * Gift products are added to the cart after Cart Price Rule is applied and only 1 item of specific gift product can be in the cart.
+     * Because during the cart merge, Magento adds quantities of items we have to change quantity of gift product to 1.
      * @param Observer $observer
      * @return void
      */
