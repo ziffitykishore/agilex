a5caff52a4f3a3d585e8bad72e511419014f26c0
ajith <ajithkumar.maragathavel@ziffity.com>
2019-07-08 15:18:46 +0530

    Image patch implemented and shipping label changed

---
+++ app/code/Ziffity/DuplicateImagePatch/registration.php
@@ -0,0 +1,8 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ziffity_DuplicateImagePatch',
+    __DIR__
+);
+
