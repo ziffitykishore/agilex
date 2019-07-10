4c89cab7c8c835252701dc74e0ab9bfdc8392214
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-07-10 21:29:57 +0200

    Fix addresses on checkout

--- app/code/SomethingDigital/CheckoutAddress/view/frontend/web/js/model/customer/address-mixin.js
+++ app/code/SomethingDigital/CheckoutAddress/view/frontend/web/js/model/customer/address-mixin.js
@@ -7,16 +7,20 @@ define(['mage/utils/wrapper'], function (wrapper) {
             
             var obj = originalAction(addressData);
             obj.getType = function () {
-                if (addressData['custom_attributes']['is_billing']['value'] == 0)
-                    return 'customer-shipping-address';
-                else
-                    return 'customer-address';
+                if (addressData['custom_attributes'].length != 0) {
+                    if (addressData['custom_attributes']['is_billing']['value'] == 0)
+                        return 'customer-shipping-address';
+                    else
+                        return 'customer-address';
+                }
             }
             obj.canUseForBilling = function () {
-                if (addressData['custom_attributes']['is_billing']['value'] == 0)
-                    return false;
-                else
-                    return true;
+                if (addressData['custom_attributes'].length != 0) {
+                    if (addressData['custom_attributes']['is_billing']['value'] == 0)
+                        return false;
+                    else
+                        return true;
+                }
             }
             return obj;
         });
