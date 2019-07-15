10fea92478ea33c1e23c034b0f9764883a69cff5
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-07-15 18:33:44 +0200

    Get addressOptions in billing-address-mixin

--- app/code/SomethingDigital/CheckoutAddress/view/frontend/web/js/view/billing-address-mixin.js
+++ app/code/SomethingDigital/CheckoutAddress/view/frontend/web/js/view/billing-address-mixin.js
@@ -1,8 +1,18 @@
-define([], function () {
+define([
+    'Magento_Customer/js/model/address-list'
+],
+function (
+    addressList
+) {
     'use strict';    
+
+    var addressOptions = addressList().filter(function (address) {
+        return address.getType() === 'customer-address';
+    });
 
     return function (BillingAddress) {
         return BillingAddress.extend({
+            addressOptions: addressOptions,
             initialize: function () {
                 this._super();
 
