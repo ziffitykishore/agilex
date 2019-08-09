0f30c31f7fff5e668d1bf2b0dba78cd3a5ede5ea
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-08-09 13:46:23 +0200

    Remove QuoteSetupFactory

--- app/code/SomethingDigital/SearchCustomization/Setup/InstallData.php
+++ app/code/SomethingDigital/SearchCustomization/Setup/InstallData.php
@@ -5,30 +5,12 @@ use Magento\Framework\DB\Ddl\Table;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Quote\Setup\QuoteSetupFactory;
  
 /**
  * @codeCoverageIgnore
  */
 class InstallData implements InstallDataInterface
 {
-    /**
-     * @var QuoteSetupFactory
-     */
-    private $quoteSetupFactory;
- 
- 
-    /**
-     * InstallData constructor.
-     * @param QuoteSetupFactory $quoteSetupFactory
-     */
-    public function __construct(
-        QuoteSetupFactory $quoteSetupFactory
-    )
-    {
-        $this->quoteSetupFactory = $quoteSetupFactory;
-    }
- 
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     { 
         $setup->startSetup();
