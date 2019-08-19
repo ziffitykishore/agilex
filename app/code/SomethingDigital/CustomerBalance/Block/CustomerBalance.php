9b01b52254063bb7fddd3fe70fae72ee4316566f
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-08-19 18:04:32 +0200

    Add customer balance block and view

---
+++ app/code/SomethingDigital/CustomerBalance/Block/CustomerBalance.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SomethingDigital\CustomerBalance\Block;
+
+use Psr\Log\LoggerInterface;
+use Magento\Framework\Stdlib\ArrayManager;
+use Magento\Framework\View\Element\Template\Context;
+use SomethingDigital\CustomerBalance\Model\CustomerBalanceApi;
+
+
+class CustomerBalance extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var ArrayManager
+     */
+    private $arrayManager;
+
+    /**
+     * @var CustomerBalanceApi
+     */
+    private $customerBalanceApi
+    
+
+    public function __construct(
+        Context $context,
+        LoggerInterface $logger,
+        ArrayManager $arrayManager,
+        CustomerBalanceApi $customerBalanceApi,
+        array $data = []
+    ) {
+        $this->logger = $logger;
+        $this->arrayManager = $arrayManager;
+        $this->customerBalanceApi = $customerBalanceApi;
+        parent::__construct($context, $data);
+    }
+
+    public function getCustomerBalance()
+    {
+        $balance = $this->arrayManager->get('body', $this->customerBalanceApi->getCustomerBalance(), []);
+
+        return $balance;
+    }
+}
