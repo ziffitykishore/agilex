2a0aeb710041e35ce060553e9a8724ef378144eb
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-08-22 12:48:31 +0200

    Create customer attribute

---
+++ app/code/SomethingDigital/CustomShipping/Setup/InstallData.php
@@ -0,0 +1,49 @@
+<?php
+namespace SomethingDigital\CustomShipping\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Model\Config;
+use Magento\Customer\Model\Customer;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+    private $eavConfig;
+
+    public function __construct(EavSetupFactory $eavSetupFactory, Config $eavConfig)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->eavConfig       = $eavConfig;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            \Magento\Customer\Model\Customer::ENTITY,
+            'customer_freight_account',
+            [
+                'type'         => 'varchar',
+                'label'        => 'Customer Freight Account',
+                'input'        => 'text',
+                'required'     => false,
+                'visible'      => true,
+                'user_defined' => true,
+                'position'     => 999,
+                'system'       => 0,
+            ]
+        );
+        $customerAttribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'customer_freight_account');
+
+        $customerAttribute->setData(
+            'used_in_forms',
+            ['adminhtml_checkout','adminhtml_customer','customer_account_edit', 'customer_account_create']
+        );
+
+        $customerAttribute->save();
+    }
+}
