7b25d6450d9822ea1fcae79f3f71e19e09443e46
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-08-22 13:41:16 +0200

    Fix customer attribute

--- app/code/SomethingDigital/CustomShipping/Setup/InstallData.php
+++ app/code/SomethingDigital/CustomShipping/Setup/InstallData.php
@@ -8,42 +8,52 @@ use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Eav\Model\Config;
 use Magento\Customer\Model\Customer;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
 
 class InstallData implements InstallDataInterface
 {
     private $eavSetupFactory;
     private $eavConfig;
 
-    public function __construct(EavSetupFactory $eavSetupFactory, Config $eavConfig)
+    public function __construct(EavSetupFactory $eavSetupFactory, Config $eavConfig, AttributeSetFactory $attributeSetFactory)
     {
         $this->eavSetupFactory = $eavSetupFactory;
         $this->eavConfig       = $eavConfig;
+        $this->attributeSetFactory = $attributeSetFactory;
     }
 
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
-        $eavSetup->addAttribute(
-            \Magento\Customer\Model\Customer::ENTITY,
-            'customer_freight_account',
-            [
-                'type'         => 'varchar',
-                'label'        => 'Customer Freight Account',
-                'input'        => 'text',
-                'required'     => false,
-                'visible'      => true,
-                'user_defined' => true,
-                'position'     => 999,
-                'system'       => 0,
-            ]
-        );
-        $customerAttribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'customer_freight_account');
 
-        $customerAttribute->setData(
-            'used_in_forms',
-            ['adminhtml_checkout','adminhtml_customer','customer_account_edit', 'customer_account_create']
-        );
+        $customerEntity = $this->eavConfig->getEntityType('customer');
+        $attributeSetId = $customerEntity->getDefaultAttributeSetId();
 
-        $customerAttribute->save();
+        /** @var $attributeSet AttributeSet */
+        $attributeSet = $this->attributeSetFactory->create();
+        $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
+
+        $eavSetup->addAttribute(\Magento\Customer\Model\Customer::ENTITY, 'customer_freight_account', [
+            'user_defined' => true,
+            'position' => 999,
+            'type' => 'varchar',
+            'label' => 'Customer Freight Account',
+            'input' => 'text',
+            'sort_order' => 9999,
+            'global' => 1,
+            'visible' => true,
+            'required' => false,
+            'system' => false,
+            'group' => 'Account Information',
+        ]);
+
+        $attribute = $this->eavConfig->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'customer_freight_account')
+            ->addData([
+                'attribute_set_id' => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms' => ['adminhtml_checkout','adminhtml_customer','customer_account_edit', 'customer_account_create'],
+            ]);
+
+        $attribute->save();
     }
 }
