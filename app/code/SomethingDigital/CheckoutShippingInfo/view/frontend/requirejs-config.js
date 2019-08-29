d6db06929d41881b127179db128abf83a74ae6cb
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-08-29 11:31:27 +0200

    Fix 301 dd issue - move section about the address form

---
+++ app/code/SomethingDigital/CheckoutShippingInfo/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/shipping.html':
+                'SomethingDigital_CheckoutShippingInfo/template/checkout/shipping.html',
+            'Magento_NegotiableQuote/template/shipping.html':
+                'SomethingDigital_CheckoutShippingInfo/template/negotiable-quote/shipping.html'
+        }
+    }
+};
