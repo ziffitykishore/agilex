ba6438cef39f96eabca4381501a5fbda069a3590
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-09-02 15:09:01 +0200

    Override template

---
+++ app/code/SomethingDigital/ShipperHqCustomizations/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/summary/item/details.html':
+                'SomethingDigital_ShipperHqCustomizations/template/summary/item/details.html'
+        }
+    }
+};
