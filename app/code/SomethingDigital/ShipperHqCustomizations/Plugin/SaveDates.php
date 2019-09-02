4279ecbcde05ff2936b98267e9ee7e44fefa0a55
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-09-02 15:01:43 +0200

    Store expected delivery dates on rate call

---
+++ app/code/SomethingDigital/ShipperHqCustomizations/Plugin/SaveDates.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SomethingDigital\ShipperHqCustomizations\Plugin;
+
+use Magento\Framework\Session\SessionManagerInterface;
+
+class SaveDates
+{
+    protected $session;
+
+    public function __construct(
+        SessionManagerInterface $coreSession
+    ) {
+        $this->session = $coreSession;
+    }
+
+    public function afterSendAndReceive(\ShipperHQ\WS\Client\WebServiceClient $subject, $result, $requestObj, $webServiceURL, $timeout)
+    {
+        $items = [];
+        foreach ($result['result']->carrierGroups as $carrierGroup) {
+            foreach ($carrierGroup->carrierRates as $carrierRate) {
+                foreach ($carrierRate->shipments as $shipment) {
+                    foreach ($shipment->boxedItems as $item) {
+                        $items [$item->sku] = $carrierRate->deliveryDateMessage;
+                    }
+                }
+            }
+        }
+
+        $this->session->setItemsDeliveryDates($items);
+
+        return $result;
+    }
+}
