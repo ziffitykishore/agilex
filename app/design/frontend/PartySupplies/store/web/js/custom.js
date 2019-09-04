7fd383a6b7e7be26106b6e90a26c9883a5443466
Kishore Nagalingam <kishore.nagalingam@ziffity.com>
2019-09-04 16:02:44 +0530

    PSU-36_HomePage_UI_changed-search-UI

--- app/design/frontend/PartySupplies/store/web/js/custom.js
+++ app/design/frontend/PartySupplies/store/web/js/custom.js
@@ -55,7 +55,7 @@ require(['jquery', 'slick'], function ($) {
         function stickyBar(elm) {
             var elment = $(elm);
             if (elment.length) {
-                var stickyOffset = elment.offset().top + 50;
+                var stickyOffset = elment.offset().top + 100;
                 $(window).scroll(function() {
                     var sticky = elment,
                         scroll = $(window).scrollTop();
