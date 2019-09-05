dd71f540dc1fe412d4e8e52bf902c6ba029ff230
Kyle Leighton <kyle.leighton1@gmail.com>
2019-09-05 13:10:24 -0400

    Remove duplicate 'All' in 'All Products'
    Magento adds 'All' before all categories by default. One of the categories
    is called All Products, and so on mobile, we see All All Products
    
    Fixes DD 71427 155

--- app/design/frontend/Travers/default/js/global/theme.js
+++ app/design/frontend/Travers/default/js/global/theme.js
@@ -9,4 +9,5 @@ domready(() => {
     $('.contact-link').first().clone().appendTo('.navigation ul:first');
     $('.about-link').first().clone().appendTo('.navigation ul:first');
     $('.phone-number.only-on-mobile').first().clone().prependTo('.page-header');
+    $('.level-top.first').click(() => $('.all-category a').text('All Products'))
 });
