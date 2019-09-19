c77d9c1ca5ee2db1f1feb83304b3b4c3529cfef2
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-09-19 18:37:38 +0200

    Implement modal to show stock data

---
+++ app/code/SomethingDigital/StockInfo/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_QuickOrder/templates/product-info.html':
+                'SomethingDigital_StockInfo/template/product-info.html'
+        }
+    }
+};
