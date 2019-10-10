a7d2a1ea016b1fddcc4396644911176a8679c59d
ramesh <ramesh.durairajaram@ziffity.com>
2019-10-10 16:19:34 +0530

    Merged modules and removed unwanted code

--- app/code/PartySupplies/Catalog/ViewModel/Product.php
+++ app/code/PartySupplies/Catalog/ViewModel/Product.php
@@ -215,4 +215,27 @@ class Product implements ArgumentInterface
 
         return $children[$minQtyIndex]['msrp'];
     }
+
+    /**
+     * @param type $productId
+     * @param type $websiteId
+     * @param array $validators
+     * @return JSON
+     */
+    public function getQuantityValidators($productId, $websiteId, array $validators = [])
+    {
+        $stockItem = $this->stockRegistry->getStockItem($productId, $websiteId);
+
+        $params = [];
+        $params['minAllowed']  = (float)$stockItem->getMinSaleQty();
+        if ($stockItem->getMaxSaleQty()) {
+            $params['maxAllowed'] = (float)$stockItem->getMaxSaleQty();
+        }
+        if ($stockItem->getQtyIncrements() > 0) {
+            $params['qtyIncrements'] = (float)$stockItem->getQtyIncrements();
+        }
+        $validators['validate-item-quantity'] = $params;
+
+        return json_encode($validators);
+    }
 }
--- app/code/PartySupplies/Common/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'PartySupplies_Common',
-    __DIR__
-);
