06d22e902232b333c6c51d2a2a142a8502d8d95b
Adrian Sobkowski <asobkowski@somethingdigital.com>
2019-10-11 16:54:04 +0200

    Remove credit balance

---
+++ app/code/SomethingDigital/CompanyCredit/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_CompanyCredit/template/payment/companycredit-form.html':
+                'SomethingDigital_CompanyCredit/template/payment/companycredit-form.html',
+        }
+    }
+};
