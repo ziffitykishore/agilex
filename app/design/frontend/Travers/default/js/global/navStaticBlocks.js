e011f5eb2e71f844ddd99b8e29f74c6b6cc0d96e
Kyle Leighton <kyle.leighton1@gmail.com>
2019-10-24 15:01:58 -0400

    Add mobile static block JS

--- app/design/frontend/Travers/default/js/global/navStaticBlocks.js
+++ app/design/frontend/Travers/default/js/global/navStaticBlocks.js
@@ -3,19 +3,32 @@ import domready from 'domready';
 
 const showStaticBlockSubmenu = () => {
   const $levelTop = $('.level-top.parent');
-  const showSubmenuClass = 'show-submenu';
+  const showSubmenuClassDesktop = 'desktop-show-submenu';
   const showSubmenuSpeed = 300;
 
-  $levelTop.mouseenter(function () {
+  // Desktop
+  $levelTop.mouseenter(function() {
     const staticBlockSubmenu = $(this).find('.static-block-submenu');
 
-    if (staticBlockSubmenu) {
-      setTimeout(() => staticBlockSubmenu.addClass(showSubmenuClass), showSubmenuSpeed);
+    if (staticBlockSubmenu.length == 1) {
+      setTimeout(() => staticBlockSubmenu.addClass(showSubmenuClassDesktop), showSubmenuSpeed);
     }
   });
 
   $levelTop.mouseleave(() => {
-    setTimeout(() => $(`.${showSubmenuClass}`).removeClass(showSubmenuClass), showSubmenuSpeed);
+    setTimeout(() => $(`.${showSubmenuClassDesktop}`).removeClass(showSubmenuClassDesktop), showSubmenuSpeed);
+  });
+
+  // Mobile
+  $levelTop.click(function(e) {
+    const staticBlockSubmenu = $(this).find('.static-block-submenu');
+
+    if (staticBlockSubmenu.length == 1) {
+      e.preventDefault();
+      e.stopPropagation();
+      staticBlockSubmenu.toggleClass('mobile-show-submenu');
+      $(this).find('a[role="menuitem"]').toggleClass('ui-state-active');
+    }
   });
 }
 
