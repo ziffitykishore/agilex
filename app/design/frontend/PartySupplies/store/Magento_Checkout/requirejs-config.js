726feaf653348ebf7622f73f22ed7f0f3fa6a5ed
ramesh <ramesh.durairajaram@ziffity.com>
2019-10-24 18:01:53 +0530

    PSU 89 : Added AddressBook functionality

--- app/design/frontend/PartySupplies/store/Magento_Checkout/requirejs-config.js
+++ app/design/frontend/PartySupplies/store/Magento_Checkout/requirejs-config.js
@@ -3,6 +3,9 @@ var config = {
         mixins: {
             'Magento_Checkout/js/view/shipping': {
                 'Magento_Checkout/js/view/shipping-mixin': true
+            },
+            'Magento_Checkout/js/view/shipping-address/address-renderer/default': {
+                'Magento_Checkout/js/view/shipping-address/address-renderer/default-mixin': true
             }
         }
     }
