8dcd22cf646fe948a90367c90f476b2c11210247
Ajith <ajithkumar.maragathavel@ziffity.com>
2019-10-26 16:50:26 +0530

    Copied DefaultItem method from vendor

--- app/code/PartySupplies/Checkout/CustomerData/DefaultItem.php
+++ app/code/PartySupplies/Checkout/CustomerData/DefaultItem.php
@@ -49,4 +49,33 @@ class DefaultItem extends Item
         $this->escaper = $escaper ?: ObjectManager::getInstance()->get(\Magento\Framework\Escaper::class);
         $this->itemResolver = $itemResolver ?: ObjectManager::getInstance()->get(ItemResolverInterface::class);
     }
+
+    protected function doGetItemData()
+    {
+        $imageHelper = $this->imageHelper->init($this->getProductForThumbnail(), 'mini_cart_product_thumbnail');
+        $productName = $this->escaper->escapeHtml($this->item->getProduct()->getName());
+
+        return [
+            'options' => $this->getOptionList(),
+            'qty' => $this->item->getQty() * 1,
+            'item_id' => $this->item->getId(),
+            'configure_url' => $this->getConfigureUrl(),
+            'is_visible_in_site_visibility' => $this->item->getProduct()->isVisibleInSiteVisibility(),
+            'product_id' => $this->item->getProduct()->getId(),
+            'product_name' => $productName,
+            'product_sku' => $this->item->getProduct()->getSku(),
+            'product_url' => $this->getProductUrl(),
+            'product_has_url' => $this->hasProductUrl(),
+            'product_price' => $this->checkoutHelper->formatPrice($this->item->getCalculationPrice()),
+            'product_price_value' => $this->item->getCalculationPrice(),
+            'product_image' => [
+                'src' => $imageHelper->getUrl(),
+                'alt' => $imageHelper->getLabel(),
+                'width' => $imageHelper->getWidth(),
+                'height' => $imageHelper->getHeight(),
+            ],
+            'canApplyMsrp' => $this->msrpHelper->isShowBeforeOrderConfirm($this->item->getProduct())
+                && $this->msrpHelper->isMinimalPriceLessMsrp($this->item->getProduct()),
+        ];
+    }
 }
