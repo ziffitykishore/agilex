1977716567e5bd175284b415e790bff9c1993ce2
Kyle Leighton <kyle.leighton1@gmail.com>
2019-10-31 09:49:09 -0400

    Account for a numbers section with the title '#'
    Fixes DD 71427 348

--- app/design/frontend/Travers/default/js/global/brands.js
+++ app/design/frontend/Travers/default/js/global/brands.js
@@ -1,15 +1,25 @@
 import $ from 'jquery';
 
+const numbersId = 'numbers';
+
 const assignIdsToColumnTitles = () => {
   $('.pagebuilder-collapsible').each(function () {
     const collapsibleTitle = $(this).find('[data-role="title"]');
     collapsibleTitle.attr('id', collapsibleTitle.text().toLowerCase());
+
+    if (collapsibleTitle.text() === '#') {
+      collapsibleTitle.attr('id', numbersId);
+    }
   });
 }
 
 const wrapNavItemsInAnchorTag = () => {
   $('.brands-nav [data-content-type="text"] p').each(function () {
-    $(this).wrap(`<a href=#${$(this).text().toLowerCase()}></a>`)
+    if ($(this).text() === '#') {
+      $(this).wrap(`<a href='#${numbersId}'></a>`)
+    } else {
+      $(this).wrap(`<a href=#${$(this).text().toLowerCase()}></a>`)
+    }
   });
 }
 
