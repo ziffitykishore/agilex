00f406a92185d54bad918544bccac7af628a50f3
Ajith <ajithkumar.maragathavel@ziffity.com>
2019-11-04 13:52:09 +0530

    Coding standard implemented for WeltPixel Category class

--- app/code/PartySupplies/LayeredNavigation/Model/Layer/Filter/Category.php
+++ app/code/PartySupplies/LayeredNavigation/Model/Layer/Filter/Category.php
@@ -8,17 +8,25 @@ use WeltPixel\LayeredNavigation\Model\Layer\Filter as LayerFilter;
 
 class Category extends AbstractFilter
 {
-	/** @var \WeltPixel\LayeredNavigation\Helper\Data */
-	protected $_moduleHelper;
+    /**
+     * @var \WeltPixel\LayeredNavigation\Helper\Data
+     */
+    protected $_moduleHelper;
 
-	/** @var bool Is Filterable Flag */
-	protected $_isFilter = false;
+    /**
+     * @var bool Is Filterable Flag
+     */
+    protected $_isFilter = false;
 
-	/** @var \Magento\Framework\Escaper */
-	private $escaper;
+    /**
+     * @var \Magento\Framework\Escaper
+     */
+    private $escaper;
 
-	/** @var  \Magento\Catalog\Model\Layer\Filter\DataProvider\Category */
-	private $dataProvider;
+    /**
+     * @var  \Magento\Catalog\Model\Layer\Filter\DataProvider\Category
+     */
+    private $dataProvider;
 
     /**
      * Category constructor.
@@ -32,111 +40,110 @@ class Category extends AbstractFilter
      * @param LayerFilter $layerFilter
      * @param array $data
      */
-	public function __construct(
-		\Magento\Catalog\Model\Layer\Filter\ItemFactory $filterItemFactory,
-		\Magento\Store\Model\StoreManagerInterface $storeManager,
-		\Magento\Catalog\Model\Layer $layer,
-		\Magento\Catalog\Model\Layer\Filter\Item\DataBuilder $itemDataBuilder,
-		\Magento\Framework\Escaper $escaper,
-		\Magento\Catalog\Model\Layer\Filter\DataProvider\CategoryFactory $categoryDataProviderFactory,
-		LayerHelper $moduleHelper,
+    public function __construct(
+        \Magento\Catalog\Model\Layer\Filter\ItemFactory $filterItemFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Catalog\Model\Layer $layer,
+        \Magento\Catalog\Model\Layer\Filter\Item\DataBuilder $itemDataBuilder,
+        \Magento\Framework\Escaper $escaper,
+        \Magento\Catalog\Model\Layer\Filter\DataProvider\CategoryFactory $categoryDataProviderFactory,
+        LayerHelper $moduleHelper,
         LayerFilter $layerFilter,
-		array $data = []
-	)
-	{
-		parent::__construct(
-			$filterItemFactory,
-			$storeManager,
-			$layer,
-			$itemDataBuilder,
-			$escaper,
-			$categoryDataProviderFactory,
-			$data
-		);
-
-		$this->escaper       = $escaper;
-		$this->_moduleHelper = $moduleHelper;
+        array $data = []
+    )
+    {
+        parent::__construct(
+            $filterItemFactory,
+            $storeManager,
+            $layer,
+            $itemDataBuilder,
+            $escaper,
+            $categoryDataProviderFactory,
+            $data
+        );
+        $this->escaper       = $escaper;
+        $this->_moduleHelper = $moduleHelper;
         $this->_layerFilter  = $layerFilter;
-		$this->dataProvider  = $categoryDataProviderFactory->create(['layer' => $this->getLayer()]);
-	}
+        $this->dataProvider  = $categoryDataProviderFactory->create(['layer' => $this->getLayer()]);
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function apply(\Magento\Framework\App\RequestInterface $request)
-	{
-		if (!$this->_moduleHelper->isEnabled()) {
-			return parent::apply($request);
-		}
+    /**
+     * @inheritdoc
+     */
+    public function apply(\Magento\Framework\App\RequestInterface $request)
+    {
+        if (!$this->_moduleHelper->isEnabled()) {
+            return parent::apply($request);
+        }
 
-		$categoryId = $request->getParam($this->_requestVar);
-		if (empty($categoryId)) {
-			return $this;
-		}
+        $categoryId = $request->getParam($this->_requestVar);
+        if (empty($categoryId)) {
+            return $this;
+        }
 
-		$categoryIds = [];
-		foreach (explode(',', $categoryId) as $key => $id) {
-			$this->dataProvider->setCategoryId($id);
-			if ($this->dataProvider->isValid()) {
-				$category = $this->dataProvider->getCategory();
-				if ($request->getParam('id') != $id) {
-					$categoryIds[] = $id;
-					$this->getLayer()->getState()->addFilter($this->_createItem($category->getName(), $id));
-				}
-			}
-		}
+        $categoryIds = [];
+        foreach (explode(',', $categoryId) as $key => $id) {
+            $this->dataProvider->setCategoryId($id);
+            if ($this->dataProvider->isValid()) {
+                $category = $this->dataProvider->getCategory();
+                if ($request->getParam('id') != $id) {
+                    $categoryIds[] = $id;
+                    $this->getLayer()->getState()->addFilter($this->_createItem($category->getName(), $id));
+                }
+            }
+        }
 
-		if (sizeof($categoryIds)) {
-			$this->_isFilter = true;
-			$this->getLayer()->getProductCollection()->addLayerCategoryFilter($categoryIds);
-		}
+        if (sizeof($categoryIds)) {
+            $this->_isFilter = true;
+            $this->getLayer()->getProductCollection()->addLayerCategoryFilter($categoryIds);
+        }
 
-		if ($parentCategoryId = $request->getParam('id')) {
-			$this->dataProvider->setCategoryId($parentCategoryId);
-		}
+        if ($parentCategoryId = $request->getParam('id')) {
+            $this->dataProvider->setCategoryId($parentCategoryId);
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
     /**
      * @return array
      * @throws \Magento\Framework\Exception\StateException
      */
-	protected function _getItemsData()
-	{
-		if (!$this->_moduleHelper->isEnabled()) {
-			return parent::_getItemsData();
-		}
+    protected function _getItemsData()
+    {
+        if (!$this->_moduleHelper->isEnabled()) {
+            return parent::_getItemsData();
+        }
 
-		/** @var \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection $productCollection */
-		$productCollection = $this->getLayer()->getProductCollection();
+        /** @var \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection $productCollection */
+        $productCollection = $this->getLayer()->getProductCollection();
 
-		if ($this->_isFilter) {
-			$productCollection = $productCollection->getCollectionClone()
-				->removeAttributeSearch('category_ids');
-		}
+        if ($this->_isFilter) {
+            $productCollection = $productCollection->getCollectionClone()
+                ->removeAttributeSearch('category_ids');
+        }
 
-		$optionsFacetedData = $productCollection->getFacetedData('category');
-		$category           = $this->dataProvider->getCategory();
-		$categories         = $category->getChildrenCategories();
+        $optionsFacetedData = $productCollection->getFacetedData('category');
+        $category           = $this->dataProvider->getCategory();
+        $categories         = $category->getChildrenCategories();
 
-		$collectionSize = $productCollection->getSize();
+        $collectionSize = $productCollection->getSize();
 
-		if ($category->getIsActive()) {
-			foreach ($categories as $category) {
-				$count = isset($optionsFacetedData[$category->getId()]) ? $optionsFacetedData[$category->getId()]['count'] : 0;
-				if ($category->getIsActive()
-					&& $this->_layerFilter->isOptionReducesResults($this, $count, $collectionSize)
-				) {
-					$this->itemDataBuilder->addItemData(
-						$this->escaper->escapeHtml($category->getName()),
-						$category->getId(),
-						$count
-					);
-				}
-			}
-		}
+        if ($category->getIsActive()) {
+            foreach ($categories as $category) {
+                $count = isset($optionsFacetedData[$category->getId()]) ? $optionsFacetedData[$category->getId()]['count'] : 0;
+                if ($category->getIsActive()
+                    && $this->_layerFilter->isOptionReducesResults($this, $count, $collectionSize)
+                ) {
+                    $this->itemDataBuilder->addItemData(
+                        $this->escaper->escapeHtml($category->getName()),
+                        $category->getId(),
+                        $count
+                    );
+                }
+            }
+        }
 
-		return $this->itemDataBuilder->build();
-	}
+        return $this->itemDataBuilder->build();
+    }
 }
