2b3627d9e4a5a98295f44817c9695a859549a7fe
Kyle Leighton <kyle.leighton1@gmail.com>
2019-11-06 16:09:44 -0500

    Hide details box if there are no details
    Fixes DD 71427 431

--- app/design/frontend/Travers/default/js/pdp/index.js
+++ app/design/frontend/Travers/default/js/pdp/index.js
@@ -1,1 +1,9 @@
 import "./preselect";
+import $ from 'jquery';
+import domready from 'domready';
+
+domready(() => {
+  if ($('.detail-block-description').find('div').length === 0) {
+    $('.detail-block-description').hide();
+  }
+});
