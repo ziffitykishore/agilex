9f6915e661be89b9f30883a206677790e02f8fe7
Kyle Leighton <kyle.leighton1@gmail.com>
2019-11-07 12:47:55 -0500

    Ensure clicking any review link takes you to the reviews form
    None of the links (be the first to review, add a review, 2 reviews) did anything
    
    Fixes DD 71427 439

--- app/design/frontend/Travers/default/js/pdp/index.js
+++ app/design/frontend/Travers/default/js/pdp/index.js
@@ -1,1 +1,10 @@
 import "./preselect";
+import $ from 'jquery';
+
+$(document).on('click', '.product-info-price .action', function (e) {
+  e.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $('.review-fieldset').offset().top - 40
+  }, 500);
+});
