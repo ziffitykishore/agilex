710f125127732f1e13377b82c724eee3d79fa82d
Kyle Leighton <kyle.leighton1@gmail.com>
2019-11-07 16:16:14 -0500

    Move content into domready after merge

--- app/design/frontend/Travers/default/js/pdp/index.js
+++ app/design/frontend/Travers/default/js/pdp/index.js
@@ -2,16 +2,16 @@ import "./preselect";
 import $ from 'jquery';
 import domready from 'domready';
 
-$(document).on('click', '.product-info-price .action', function (e) {
-  e.preventDefault();
-
-  $('html, body').animate({
-    scrollTop: $('.review-fieldset').offset().top - 40
-  }, 500);
-});
-
 domready(() => {
   if ($('.detail-block-description').find('div').length === 0) {
     $('.detail-block-description').hide();
   }
+
+  $(document).on('click', '.product-info-price .action', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $('.review-fieldset').offset().top - 40
+    }, 500);
+  });
 });
