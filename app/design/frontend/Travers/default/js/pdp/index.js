70a906763bb03d0db7bdf83f81996bfb9043d8dc
Kyle Leighton <kyle.leighton1@gmail.com>
2019-11-08 10:40:53 -0500

    Set the quantity field to the minimum increment if there is one
    Fixes DD 71427 462

--- app/design/frontend/Travers/default/js/pdp/index.js
+++ app/design/frontend/Travers/default/js/pdp/index.js
@@ -2,8 +2,22 @@ import "./preselect";
 import $ from 'jquery';
 import domready from 'domready';
 
+const setQtyToMinimumIncrement = () => {
+  const minimumIncrementText = $('#product_addtocart_form').find('.product.pricing').text();
+  const minimumIncrementValue = minimumIncrementText.trim().split(' ').pop();
+  const quantityInput = $('#product_addtocart_form').find('#qty');
+
+  if (!minimumIncrementText) {
+    return;
+  }
+
+  quantityInput.val(minimumIncrementValue);
+}
+
 domready(() => {
   if ($('.detail-block-description').find('div').length === 0) {
     $('.detail-block-description').hide();
   }
+
+  setQtyToMinimumIncrement();
 });
