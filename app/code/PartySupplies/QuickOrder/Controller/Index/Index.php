15184ce7a44b326cf13f6868382dd1f185d35ad8
Ajith <ajithkumar.maragathavel@ziffity.com>
2019-11-10 13:20:29 +0530

    Preference added for Index Controller

---
+++ app/code/PartySupplies/QuickOrder/Controller/Index/Index.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace PartySupplies\QuickOrder\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+use Mageplaza\QuickOrder\Helper\Data;
+
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @var Data
+     */
+    protected $_helperData;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     * @param Data $helperData
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        Data $helperData
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->_helperData = $helperData;
+
+        parent::__construct($context);
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute()
+    {
+        $storeId = $this->_helperData->getStore()->getStoreId();
+        $identifier = trim($this->_request->getPathInfo(), '/');
+        if ($this->_helperData->checkPermissionAccess() === false
+            || in_array($identifier, ['quickorder', 'quickorder/index/index'])) {
+            $this->_redirect('cms/noroute/');
+        }
+
+        /** @var Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->addHandle('quickorder_index_index');
+        $resultPage->getConfig()->getTitle()->set($this->_helperData->getPageTitle($storeId));
+
+        return $resultPage;
+    }
+}
