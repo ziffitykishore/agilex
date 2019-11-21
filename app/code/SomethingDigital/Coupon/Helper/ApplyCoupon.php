60d26e21ffd741b071ac595cb0b37396140c428c
asobkowski <asobkowski@somethingdigital.com>
2019-11-21 15:40:15 +0100

    Fix removing coupon cookie

--- app/code/SomethingDigital/Coupon/Helper/ApplyCoupon.php
+++ app/code/SomethingDigital/Coupon/Helper/ApplyCoupon.php
@@ -31,7 +31,7 @@ class ApplyCoupon extends \Magento\Framework\App\Helper\AbstractHelper
                 ->collectTotals()
                 ->save();
 
-            $this->cookieManager->deleteCookie('coupon');
+            setcookie('coupon', '', time() - 3600, '/');
 
             if (!$coupon->getCouponCode()) {
                 //if no coupon code is found set coupon code as suffix value
