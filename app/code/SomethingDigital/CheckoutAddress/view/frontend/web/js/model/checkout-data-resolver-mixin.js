bb01327b465f7b93d085a96fe52b10b218909c87
asobkowski <asobkowski@somethingdigital.com>
2019-12-15 06:56:40 +0100

    Automatically select the default billing address for the user

--- app/code/SomethingDigital/CheckoutAddress/view/frontend/web/js/model/checkout-data-resolver-mixin.js
+++ app/code/SomethingDigital/CheckoutAddress/view/frontend/web/js/model/checkout-data-resolver-mixin.js
@@ -52,6 +52,15 @@ define([
                 });
             }
 
+            addressList.some(function (addrs) {
+                if (addrs.customAttributes.hasOwnProperty('is_billing')) {
+                    if (addrs.customAttributes.is_billing.value == 1) {
+                        selectBillingAddress(addrs);
+                        return true;
+                    }
+                }
+            });
+
             shippingAddress = quote.shippingAddress();
         };
 
