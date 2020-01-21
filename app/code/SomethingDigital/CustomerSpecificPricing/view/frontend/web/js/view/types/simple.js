b1b5990dccf1cc113772a993688fdaa43c993472
asobkowski <asobkowski@somethingdigital.com>
2020-01-21 15:59:58 +0100

    Fix customer special pricing for simple products

--- app/code/SomethingDigital/CustomerSpecificPricing/view/frontend/web/js/view/types/simple.js
+++ app/code/SomethingDigital/CustomerSpecificPricing/view/frontend/web/js/view/types/simple.js
@@ -3,10 +3,13 @@ define([
 ], function ($) {
     return function (data, currencySymbol) {
         var sku = $('[itemprop=sku]').text();
-        var price = data[sku]['price'];
 
-        if (price != null && price != 0) {
-            $('.product-info-main div.price-final_price > span:not(.old-price) .price').text(currencySymbol + price);
+        if (data[sku]['price'] !== undefined) {
+            var price = data[sku]['price'];
+
+            if (price !== null && price !== 0) {
+                $('.product-info-main div.price-final_price > span:not(.old-price) .price').text(currencySymbol + price);
+            }
         }
     }
 });
