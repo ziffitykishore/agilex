1fe30c9f6b7d0cd82e4e51ae697012164d1beddb
Itai Flam <iflam@somethingdigital.com>
2020-01-22 16:24:58 -0500

    Rename variables, add clarifications in phpdocs

--- app/code/SomethingDigital/ReactPlp/Helper/AttributeSorter.php
+++ app/code/SomethingDigital/ReactPlp/Helper/AttributeSorter.php
@@ -22,49 +22,52 @@ class AttributeSorter
      * of the array, or if the array is smaller than 9,
      * the last element of the array.
      *
+     * The assumption of a maximum of 9 to display
+     * was not made here, that can be found in:
+     * TraversTool-ReactPLP/src/Products/TableView/Table/Hits.js
+     *
      * @param array $tableAttributes current attributes
      * @param AttributeSorter::DEFAULT_ATTRIBUTES|AttributeSorter::CUSTOM_ATTRIBUTES $type whether this is for the default attributes or the custom attributes.
-     * @return array $newAttributes
+     * @return array
      */
-    public function sort(array $tableAttributes, $type)
+    public function sort($tableAttributes, $type)
     {
-        $newAttributes = $tableAttributes;
-        $priceObj = $type['price'];
-        $skuObj = $type['sku'];
+        $priceValue = $type['price'];
+        $skuValue = $type['sku'];
         //Find position of SKU in the array
-        $skuPos = array_search(
-            $skuObj,
-            $newAttributes
+        $skuPosition = array_search(
+            $skuValue,
+            $tableAttributes
         );
 
-        if ($skuPos !== 0) { //either skuPos is false (non-existent) or is greater than 0
-            if ($skuPos > 0) {
+        if ($skuPosition !== 0) { //either skuPosition is false (non-existent) or is greater than 0
+            if ($skuPosition > 0) {
                 //If SKU exists in the array and it's not in the first position we want to remove it.
-                array_splice($newAttributes, $skuPos, 1);
+                array_splice($tableAttributes, $skuPosition, 1);
             }
 
             //Add sku at beginning of array.
-            array_unshift($newAttributes, $skuObj);
+            array_unshift($tableAttributes, $skuValue);
         }
         //Find position of Price in the array
-        $pricePos = array_search(
-            $priceObj,
-            $newAttributes
+        $pricePosition = array_search(
+            $priceValue,
+            $tableAttributes
         );
 
         //Find position that Price must be in the array.
-        $pricePushPos = min(8, sizeof($newAttributes));
+        $pricePushPos = min(8, sizeof($tableAttributes));
 
-        if ($pricePos !== $pricePushPos) {
-            if ($pricePos && $pricePos >= 0) {
+        if ($pricePosition !== $pricePushPos) {
+            if ($pricePosition && $pricePosition >= 0) {
                 //If Price is not in the position it must be, we remove the old position.
-                array_splice($newAttributes, $pricePos, 1);
+                array_splice($tableAttributes, $pricePosition, 1);
             }
 
             //Add Price to required array position.
-            array_splice($newAttributes, $pricePushPos, 0, [$priceObj]);
+            array_splice($tableAttributes, $pricePushPos, 0, [$priceValue]);
         }
 
-        return $newAttributes;
+        return $tableAttributes;
     }
 }
