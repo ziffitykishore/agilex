0c05d46ddeac75504f104585b96692f16dc9a89a
asobkowski <asobkowski@somethingdigital.com>
2020-02-03 11:58:41 +0100

    Mark is_billing = true for new address id there is no billing addresss

--- app/code/SomethingDigital/CheckoutAddress/view/frontend/requirejs-config.js
+++ app/code/SomethingDigital/CheckoutAddress/view/frontend/requirejs-config.js
@@ -34,6 +34,9 @@ var config = {
             'Magento_Checkout/js/action/create-billing-address': {
                 'SomethingDigital_CheckoutAddress/js/action/create-billing-address-mixin': true
             },
+            'Magento_Checkout/js/action/create-shipping-address': {
+                'SomethingDigital_CheckoutAddress/js/action/create-shipping-address-mixin': true
+            },
             'Magento_Checkout/js/model/checkout-data-resolver': {
                 'SomethingDigital_CheckoutAddress/js/model/checkout-data-resolver-mixin': true
             },
