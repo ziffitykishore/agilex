ce2c08ab968493d9178e728993b30752c5fd1c16
asobkowski <asobkowski@somethingdigital.com>
2020-02-06 20:56:00 +0100

    Skip options if doesn't exist and return [] if category doesn't exist

--- app/code/SomethingDigital/CategoryAttributes/Controller/Grouping/View.php
+++ app/code/SomethingDigital/CategoryAttributes/Controller/Grouping/View.php
@@ -2,6 +2,7 @@
 namespace SomethingDigital\CategoryAttributes\Controller\Grouping;
  
 use Magento\Framework\UrlFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
  
 class View extends \Magento\Framework\App\Action\Action
 {
@@ -51,37 +52,42 @@ class View extends \Magento\Framework\App\Action\Action
         $cid = $this->getRequest()->getParam('id');
         try {
             $category = $this->categoryRepository->get($cid, $this->storeManager->getStore()->getId());
+        } catch (NoSuchEntityException $e) {
+            throw new \Magento\Framework\Exception\NotFoundException(__('Category does not exists.'));
+        }
         
-            $attrArray = array();
-            $data = array();
-            $attrArray []= $category->getGroupingAttribute1();
-            $attrArray []= $category->getGroupingAttribute2();
-            $attrArray []= $category->getGroupingAttribute3();
+        $attrArray = [];
+        $data = [];
+        $attrArray []= $category->getGroupingAttribute1();
+        $attrArray []= $category->getGroupingAttribute2();
+        $attrArray []= $category->getGroupingAttribute3();
 
-            foreach ($attrArray as $key => $attrCode) {
-                if (!empty($attrCode)) {
+        foreach ($attrArray as $key => $attrCode) {
+            if (!empty($attrCode)) {
+                try {
                     $attrOptions = $this->productAttributeRepository->get($attrCode)->getOptions();
-                    foreach ($attrOptions as $option) {
-                        if (trim($option->getLabel()) != '') {
-                            $optionCode = strtolower(str_replace(' ', '-', $option->getLabel())); 
-                            $optionExists = true;
-                            try {
-                                $optionHtml = $this->blockRepository->getById('grouping_' . $attrCode . '_' . $optionCode);
-                            } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
-                                $optionExists = false;
-                            }
-                            if ($optionExists) {
-                                $filteredOptionHtml = $this->filterProvider->getPageFilter()->filter($optionHtml->getContent());
-                                $data[$attrCode][$optionCode] = $filteredOptionHtml;
-                            }
+                } catch (NoSuchEntityException $e) {
+                    continue;
+                }
+
+                foreach ($attrOptions as $option) {
+                    if (trim($option->getLabel()) != '') {
+                        $optionCode = strtolower(str_replace(' ', '-', $option->getLabel()));
+                        $optionExists = true;
+                        try {
+                            $optionHtml = $this->blockRepository->getById('grouping_' . $attrCode . '_' . $optionCode);
+                        } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
+                            $optionExists = false;
+                        }
+                        if ($optionExists) {
+                            $filteredOptionHtml = $this->filterProvider->getPageFilter()->filter($optionHtml->getContent());
+                            $data[$attrCode][$optionCode] = $filteredOptionHtml;
                         }
                     }
                 }
-            }        
-
-            $this->getResponse()->representJson($this->jsonEncoder->encode($data))->setHeader('Cache-Control', 'max-age=86400, public');
-        } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
-            throw new \Magento\Framework\Exception\NotFoundException(__('Category does not exists.')); 
+            }
         }
+
+        $this->getResponse()->representJson($this->jsonEncoder->encode($data))->setHeader('Cache-Control', 'max-age=86400, public');
     }
 }
