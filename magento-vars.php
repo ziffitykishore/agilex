10b7506c75618c4288879ddb0ed51bcba5e2c2e0
Udayakumar Sarangapani <udaya.kumar@ziffity.com>
2020-03-16 21:33:30 +0530

    EMI-3 Addition of other domains

--- magento-vars.php
+++ magento-vars.php
@@ -1,17 +1,4 @@
 <?php
-// enable, adjust and copy this code for each store you run
-// Store #0, default one
-//if (isHttpHost("example.com")) {
-//    $_SERVER["MAGE_RUN_CODE"] = "default";
-//    $_SERVER["MAGE_RUN_TYPE"] = "store";
-//}
-//function isHttpHost($host)
-//{
-//    if (!isset($_SERVER['HTTP_HOST'])) {
-//        return false;
-//    }
-//    return strpos(str_replace('---', '.', $_SERVER['HTTP_HOST']), $host) === 0;
-//}
 
 // Master
 if (isHttpHost("master-7rqtwti-kzhstxzybfg6w.us-3.magentosite.cloud")) {
@@ -20,24 +7,87 @@ if (isHttpHost("master-7rqtwti-kzhstxzybfg6w.us-3.magentosite.cloud")) {
 }
 
 // Production
-if (isHttpHost("earthlite.com.c.kzhstxzybfg6w.ent.magento.cloud")) {
-    $_SERVER["MAGE_RUN_CODE"] = "base";
-    $_SERVER["MAGE_RUN_TYPE"] = "website";
-}
-
 if (isHttpHost("mcprod.earthlite.com")) {
     $_SERVER["MAGE_RUN_CODE"] = "base";
     $_SERVER["MAGE_RUN_TYPE"] = "website";
 }
 
-// Staging
-if (isHttpHost("mcstaging.earthlite.com.c.kzhstxzybfg6w.dev.ent.magento.cloud")) {
+if (isHttpHost("mcprod.continuumpedicure.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "continuumpedicure";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("mcprod.livingearthcrafts.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "livingearthcraft";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("mcprod.taraspatherapy.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "taraspatherapy";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+/*
+if (isHttpHost("earthlite.com")) {
     $_SERVER["MAGE_RUN_CODE"] = "base";
     $_SERVER["MAGE_RUN_TYPE"] = "website";
 }
 
+if (isHttpHost("continuumpedicure.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "continuumpedicure";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("livingearthcrafts.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "livingearthcraft";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("taraspatherapy.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "taraspatherapy";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("www.earthlite.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "base";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("www.continuumpedicure.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "continuumpedicure";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("www.livingearthcrafts.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "livingearthcraft";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("www.taraspatherapy.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "taraspatherapy";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+*/
+
+// Staging
 if (isHttpHost("mcstaging.earthlite.com")) {
     $_SERVER["MAGE_RUN_CODE"] = "base";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("mcstaging.continuumpedicure.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "continuumpedicure";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("mcstaging.livingearthcrafts.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "livingearthcraft";
+    $_SERVER["MAGE_RUN_TYPE"] = "website";
+}
+
+if (isHttpHost("mcstaging.taraspatherapy.com")) {
+    $_SERVER["MAGE_RUN_CODE"] = "taraspatherapy";
     $_SERVER["MAGE_RUN_TYPE"] = "website";
 }
 
