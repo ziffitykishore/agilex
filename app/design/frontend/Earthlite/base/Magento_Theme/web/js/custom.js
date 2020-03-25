eb412fa39926e38e16fb9cfb74fc7570ce711944
Kishore N <kishore.nagalingam@ziffity.com>
2020-03-25 22:40:23 +0530

    header sticky UI

--- app/design/frontend/Earthlite/base/Magento_Theme/web/js/custom.js
+++ app/design/frontend/Earthlite/base/Magento_Theme/web/js/custom.js
@@ -9,6 +9,39 @@ define([
 ], function ($) {
     'use strict';
 
+    /* header sticky */
+
+    function sticky(navbar) {
+
+        let c, currentScrollTop = 0;
+
+        $(window).scroll(function () {
+            let a = $(window).scrollTop();
+            let b = navbar.height();
+
+            currentScrollTop = a;
+
+            if (c < currentScrollTop && a > b + b) {
+                navbar.addClass("scrollUp");
+            } else if (c > currentScrollTop && !(a <= b)) {
+                navbar.removeClass("scrollUp");
+            }
+            c = currentScrollTop;
+        });
+
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() >= 46) {
+                navbar.addClass('sticky');
+            }
+            else {
+                navbar.removeClass('sticky');
+            }
+        });
+    }
+
+    sticky($('.header-wrapper'));
+
     $('.header-right-pane > .header.links').clone().appendTo('#store\\.links');
 
 });
