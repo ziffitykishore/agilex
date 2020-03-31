e4465b6c2aa9153542808fef394332026bdc5056
sairam <sairam.sigirisetty@ziffity.com>
2020-03-31 14:05:31 +0530

    Earthlite_Customer module inital commmit

---
+++ app/code/Earthlite/Customer/Setup/Patch/Data/AddCustomerABMPAttribute.php
@@ -0,0 +1,114 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Class Data
+ *
+ */
+namespace Earthlite\Customer\Setup\Patch\Data;
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+
+/**
+ * class AddCustomerABMPAttribute
+ */
+class AddCustomerABMPAttribute implements DataPatchInterface  
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    protected $moduleDataSetup;
+
+    /**
+     * @var CustomerSetupFactory
+     */
+    protected $customerSetupFactory;
+
+    /**
+     * @var AttributeSetFactory
+     */
+    protected $attributeSetFactory;
+
+    /**
+     * AddCustomerABMPAttribute constructor
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     * @param AttributeSetFactory $attributeSetFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerEntity = $customerSetup->getEavConfig()->getEntityType(Customer::ENTITY);
+        $attributeSetId = $customerEntity->getDefaultAttributeSetId();
+
+        $attributeSet = $this->attributeSetFactory->create();
+        $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
+
+        $customerSetup->addAttribute(
+            Customer::ENTITY,
+            'abmp_number',
+            [
+                'type' => 'varchar',
+                'label' => 'ABMP Membership Number',
+                'input' => 'text',
+                'validate_rules' => '{"max_text_length":8,"min_text_length":5}',
+                'required' => false,
+                'sort_order' => 120,
+                'position' => 120,
+                'visible' => true,
+                'user_defined' => true,
+                'unique' => false,
+                'system' => false,
+            ]
+        );
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute(
+            Customer::ENTITY,
+            'abmp_number'
+        );
+
+        $attribute->addData(
+            [
+                'attribute_set_id' => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms' => ['adminhtml_customer', 'customer_account_create', 'customer_account_edit'],
+            ]
+        );
+
+        $attribute->save();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+
+}
