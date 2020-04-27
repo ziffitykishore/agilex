98e87c86dc36e027a7f9e3c2bef201916e6d4f4a
David <david.francis@ziffity.com>
2020-04-27 13:15:37 +0530

    EL-193 new and trend ui

---
+++ app/code/Earthlite/NewTrending/view/frontend/web/js/newtrendslider.js
@@ -0,0 +1,48 @@
+define(['jquery', 'slick'], function ($) {
+    "use strict";
+    $(function () {
+        let $catSlider = $('.new-trend-slider');
+
+        $catSlider.slick({
+            infinite: false,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1300,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+
+            ]
+        });
+
+    });
+});
+    
