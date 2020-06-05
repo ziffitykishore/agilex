c6fc7c20a4d765130d024fb0088e4058909c292d
asobkowski <asobkowski@somethingdigital.com>
2020-06-05 19:30:47 +0200

    Remove line

--- app/code/SomethingDigital/SearchCustomization/Plugin/PrepareAddProductsBySku.php
+++ app/code/SomethingDigital/SearchCustomization/Plugin/PrepareAddProductsBySku.php
@@ -6,7 +6,6 @@ use Magento\AdvancedCheckout\Model\Cart;
 
 class PrepareAddProductsBySku
 {
-
     public function beforePrepareAddProductsBySku(Cart $subject, $items)
     {
         foreach ($items as $key => $item) {
