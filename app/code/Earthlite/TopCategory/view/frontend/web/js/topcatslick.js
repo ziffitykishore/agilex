91fea8170bacfe8f3d00a3d10865cfe6ec43a7c6
David Francis <david.francis@ziffity.com>
2020-06-12 19:08:54 +0530

    top category, shipping info fix

--- app/code/Earthlite/TopCategory/view/frontend/web/js/topcatslick.js
+++ app/code/Earthlite/TopCategory/view/frontend/web/js/topcatslick.js
@@ -20,8 +20,8 @@ define(['jquery', 'slick'], function ($) {
                 {
                     breakpoint: 992,
                     settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
+                        slidesToShow: 3,
+                        slidesToScroll: 3
                     }
                 },
                 {
@@ -45,7 +45,7 @@ define(['jquery', 'slick'], function ($) {
 
 
             //$progressBar.css('left',  + calc+ '%');
-            if (!(calc == 100)) {
+            if (!(calc >= 100)) {
                 $progressBar.css('left', 'calc(' + calc + '% - 0px)');
             } else {
                 $progressBar.css('left', 'calc(' + calc + '% - 100px)');
