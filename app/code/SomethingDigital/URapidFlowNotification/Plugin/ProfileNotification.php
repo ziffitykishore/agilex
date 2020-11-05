e08710be4d9ed7288580b9c32fef9e5e67c9f822
asobkowski <asobkowski@somethingdigital.com>
2020-11-05 14:14:32 +0100

    Send email when urapidlow profile is still running

---
+++ app/code/SomethingDigital/URapidFlowNotification/Plugin/ProfileNotification.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SomethingDigital\URapidFlowNotification\Plugin;
+
+use Unirgy\RapidFlow\Model\Profile;
+use SomethingDigital\URapidFlowNotification\Helper\Email;
+
+/**
+ * Class OrderRepositoryPlugin
+ */
+class ProfileNotification
+{
+    /**
+     * ProfileNotification constructor
+     *
+     * @param Email $email
+     */
+    public function __construct(
+        Email $email
+    ) {
+        $this->email = $email;
+    }
+
+    /**
+     * Send notifiacation
+     */
+    public function beforeRun(Profile $subject)
+    {
+        if ($subject->isLocked()) {
+            $this->email->sendEmail($subject->getId()); //notify that is still running
+        }
+    }
+
+}
