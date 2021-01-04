55ad0448768d0fbb0383d5db7a4edf1e93753443
Udayakumar Sarangapani <udaya.kumar@ziffity.com>
2021-01-04 22:53:38 +0530

    For JS Bundling

--- .sd-post-build.sh
+++ .sd-post-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cp -R pub/static/frontend/Travers/default/en_US pub/static/frontend/Travers/default/en_US_Source
+echo $MAGENTO_CLOUD_APP_DIR
 
-./.nvm/versions/node/v8.15.1/bin/r.js -o ./build.js baseUrl="pub/static/frontend/Travers/default/en_US_Source/" dir="pub/static/frontend/Travers/default/en_US/"
+cp -R $MAGENTO_CLOUD_APP_DIR/pub/static/frontend/Travers/default/en_US $MAGENTO_CLOUD_APP_DIR/pub/static/frontend/Travers/default/en_US_Source
+
+$MAGENTO_CLOUD_APP_DIR/.nvm/versions/node/v8.15.1/bin/r.js -o $MAGENTO_CLOUD_APP_DIR/build.js baseUrl="${MAGENTO_CLOUD_APP_DIR}/pub/static/frontend/Travers/default/en_US_Source/" dir="${MAGENTO_CLOUD_APP_DIR}/pub/static/frontend/Travers/default/en_US/"
 
