966a5f92aae88f974bc68a6f9fa39cf0a32847c3
Udayakumar Sarangapani <udaya.kumar@ziffity.com>
2021-01-04 22:43:24 +0530

    For JS Bundling

--- .sd-post-build.sh
+++ .sd-post-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mv pub/static/frontend/Travers/default/en_US pub/static/frontend/Travers/default/en_US_Source
+cp -R pub/static/frontend/Travers/default/en_US pub/static/frontend/Travers/default/en_US_Source
 
 ./.nvm/versions/node/v8.15.1/bin/r.js -o ./build.js baseUrl="pub/static/frontend/Travers/default/en_US_Source/" dir="pub/static/frontend/Travers/default/en_US/"
 
