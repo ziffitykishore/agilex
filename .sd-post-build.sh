f7ff465ab22e64dee741783bff0fb6d81ce09e35
Udayakumar Sarangapani <udaya.kumar@ziffity.com>
2021-01-04 23:13:02 +0530

    For JS Bundling

--- .sd-post-build.sh
+++ .sd-post-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo $MAGENTO_CLOUD_APP_DIR/$USER
+echo $MAGENTO_CLOUD_APP_DIR/dwi66h66ybkvo_stg
 
-cp -R $MAGENTO_CLOUD_APP_DIR/$USER/pub/static/frontend/Travers/default/en_US $MAGENTO_CLOUD_APP_DIR/$USER/pub/static/frontend/Travers/default/en_US_Source
+cp -R $MAGENTO_CLOUD_APP_DIR/dwi66h66ybkvo_stg/pub/static/frontend/Travers/default/en_US $MAGENTO_CLOUD_APP_DIR/dwi66h66ybkvo_stg/pub/static/frontend/Travers/default/en_US_Source
 
-$MAGENTO_CLOUD_APP_DIR/$USER/.nvm/versions/node/v8.15.1/bin/r.js -o $MAGENTO_CLOUD_APP_DIR/$USER/build.js baseUrl="${MAGENTO_CLOUD_APP_DIR}/${USER}/pub/static/frontend/Travers/default/en_US_Source/" dir="${MAGENTO_CLOUD_APP_DIR}/${USER}/pub/static/frontend/Travers/default/en_US/"
+$MAGENTO_CLOUD_APP_DIR/dwi66h66ybkvo_stg/.nvm/versions/node/v8.15.1/bin/r.js -o $MAGENTO_CLOUD_APP_DIR/dwi66h66ybkvo_stg/build.js baseUrl="${MAGENTO_CLOUD_APP_DIR}/dwi66h66ybkvo_stg/pub/static/frontend/Travers/default/en_US_Source/" dir="${MAGENTO_CLOUD_APP_DIR}/dwi66h66ybkvo_stg/pub/static/frontend/Travers/default/en_US/"
 
