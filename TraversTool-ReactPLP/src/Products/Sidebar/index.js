09062976a134b3e158414d7fc182d8f7733067bf
nirmal <nirmal.gandhi@ziffity.com>
2021-01-18 14:46:10 +0530

    change state value and fixed issue

--- TraversTool-ReactPLP/src/Products/Sidebar/index.js
+++ TraversTool-ReactPLP/src/Products/Sidebar/index.js
@@ -119,7 +119,7 @@ class Sidebar extends PureComponent {
     if (!this.state.valueSortByAttribute || !(attribute.id in this.state.valueSortByAttribute)) {
       return orderBy(items, 'label');
     }
-
+    this.setState({[attribute.id]:items.length});
     // Avoid changing the inside of items - it makes us re-render slower.
     const sortOrders = this.state.valueSortByAttribute[attribute.id];
     const sortedItems = items.sort((a, b) => {
@@ -127,7 +127,7 @@ class Sidebar extends PureComponent {
       const sortB = sortOrders[b.label] || 999999;
       return sortA - sortB;
     });
-
+    
     return sortedItems;
   }
 
@@ -156,7 +156,7 @@ class Sidebar extends PureComponent {
             </SkeletonTheme>
           ))}
           {!this.props.isLoadingAttributes && this.props.filterAttributesInfo.map(attribute => {
-            if(this.state.valueSortByAttribute && this.state.valueSortByAttribute[attribute.id] && Object.keys(this.state.valueSortByAttribute[attribute.id]).length === 1) return null;
+            if(this.state[attribute.id] === 3) return null;
             return (
             <div className={this.accordionClasses(attribute.id)} key={attribute.id}>
               <Accordion classNames="filter-content" dropdownTitle={attribute.label || attribute.id} isDesktop={isDesktop}>
