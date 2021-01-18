95cafea4306961e02f21875a195755f429908c84
nirmal <nirmal.gandhi@ziffity.com>
2021-01-18 14:46:34 +0530

    change state value and fixed issue

--- TraversTool-ReactPLP/src/Products/Sidebar/index.js
+++ TraversTool-ReactPLP/src/Products/Sidebar/index.js
@@ -156,7 +156,7 @@ class Sidebar extends PureComponent {
             </SkeletonTheme>
           ))}
           {!this.props.isLoadingAttributes && this.props.filterAttributesInfo.map(attribute => {
-            if(this.state[attribute.id] === 3) return null;
+            if(this.state[attribute.id] === 1) return null;
             return (
             <div className={this.accordionClasses(attribute.id)} key={attribute.id}>
               <Accordion classNames="filter-content" dropdownTitle={attribute.label || attribute.id} isDesktop={isDesktop}>
