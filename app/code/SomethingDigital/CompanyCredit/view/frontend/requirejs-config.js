8bf1b8bfbe80ee09f1589362fea4d2a737f48a02
prabhakaran <prabhakaran.anbazhagan@ziffity.com>
2021-01-20 19:34:07 +0530

    Hide the message in credit limit

--- app/code/SomethingDigital/CompanyCredit/view/frontend/requirejs-config.js
+++ app/code/SomethingDigital/CompanyCredit/view/frontend/requirejs-config.js
@@ -3,6 +3,8 @@ var config = {
         '*': {
             'Magento_CompanyCredit/template/payment/companycredit-form.html':
                 'SomethingDigital_CompanyCredit/template/payment/companycredit-form.html',
-        }
+            'Magento_CompanyCredit/js/view/payment/method-renderer/companycredit':
+                'SomethingDigital_CompanyCredit/js/view/payment/method-renderer/companycredit'
+            }
     }
 };
