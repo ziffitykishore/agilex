f2cf0794f3f6f57c76a7a0d5d539c9f5e188d6d4
prabhakaran <prabhakaran.anbazhagan@ziffity.com>
2021-01-21 17:45:13 +0530

    Comment fixed

--- app/code/SomethingDigital/CompanyCredit/Model/AdditionalConfigVars.php
+++ app/code/SomethingDigital/CompanyCredit/Model/AdditionalConfigVars.php
@@ -13,7 +13,6 @@ class AdditionalConfigVars implements ConfigProviderInterface
       }
     public function getConfig()
     {    
-        // $this->_scopeConfig = $scopeConfig;
         $enable_var = $this->_scopeConfig->getValue("companycreditmessage/general/enable");
         $additionalVariables['credit_message'] = $enable_var;
         return $additionalVariables;
