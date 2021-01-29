e63a957d5dbb787449b2e37ddf5fd84718a4a04f
prabhakaran <prabhakaran.anbazhagan@ziffity.com>
2021-01-29 10:50:15 +0530

    Adding sku without hyphen to algolia

--- app/code/SomethingDigital/AlgoliaSearch/Observer/AddProductData.php
+++ app/code/SomethingDigital/AlgoliaSearch/Observer/AddProductData.php
@@ -48,6 +48,23 @@ class AddProductData implements ObserverInterface
         $this->addStockData($product, $transport);
         $this->addTierPrices($product, $transport);
         $this->addRadius($product, $transport);
+        $this->addSkuWithoutHyphen($product, $transport);
+    }
+
+    /**
+     * Add manufacturer_price attribute to algolia data
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @param \Magento\Framework\DataObject $transport
+     */
+    private function addSkuWithoutHyphen($product, $transport)
+    {
+        $algoliaProductData = $transport->getData();
+        $hypen_removed = str_replace('-','',$product->getSku());
+        $data[] =$product->getSku();
+        array_push($data,$hypen_removed);
+        $algoliaProductData['sku'] = $data;
+        $transport->setData($algoliaProductData);
     }
 
     /**
