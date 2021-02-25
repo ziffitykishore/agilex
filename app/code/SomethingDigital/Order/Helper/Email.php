2d00d1d9a7023a9584ad454d9e191d3d90f61959
Chandru Rajendran <chandru.rajendran@ziffity.com>
2021-02-25 10:57:37 +0530

    TL-1263 order confirmation email

--- app/code/SomethingDigital/Order/Helper/Email.php
+++ app/code/SomethingDigital/Order/Helper/Email.php
@@ -8,6 +8,7 @@ use Magento\Framework\Escaper;
 use Magento\Framework\Mail\Template\TransportBuilder;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Sales\Model\Order\Email\Sender\OrderSender;
 
 class Email extends \Magento\Framework\App\Helper\AbstractHelper
 {
@@ -22,7 +23,8 @@ class Email extends \Magento\Framework\App\Helper\AbstractHelper
         StateInterface $inlineTranslation,
         Escaper $escaper,
         TransportBuilder $transportBuilder,
-        ScopeConfigInterface $scopeConfig
+        ScopeConfigInterface $scopeConfig,
+        OrderSender $sender
     ) {
         parent::__construct($context);
         $this->inlineTranslation = $inlineTranslation;
@@ -30,6 +32,7 @@ class Email extends \Magento\Framework\App\Helper\AbstractHelper
         $this->transportBuilder = $transportBuilder;
         $this->logger = $context->getLogger();
         $this->scopeConfig = $scopeConfig;
+        $this->sender = $sender;
     }
 
     public function sendEmail($order, $response)
@@ -55,6 +58,41 @@ class Email extends \Magento\Framework\App\Helper\AbstractHelper
                 ])
                 ->setFrom($sender)
                 ->addTo($this->scopeConfig->getValue('trans_email/ident_support/email',ScopeInterface::SCOPE_STORE))
+                ->getTransport();
+            $transport->sendMessage();
+            $this->inlineTranslation->resume();
+        } catch (\Exception $e) {
+            $this->logger->debug($e->getMessage());
+        }
+    }
+
+    public function sendSuccessEmail($order)
+    {
+        try {
+            $templateId = $this->scopeConfig->getValue('sales/sx_order/email_template');
+            $this->inlineTranslation->suspend();
+            $sender = [
+                'name' => $this->scopeConfig->getValue('trans_email/ident_support/name',ScopeInterface::SCOPE_STORE),
+                'email' => $this->scopeConfig->getValue('trans_email/ident_support/email', ScopeInterface::SCOPE_STORE),
+            ];
+            $transport = $this->transportBuilder
+                ->setTemplateIdentifier($templateId)
+                ->setTemplateOptions(
+                    [
+                        'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
+                        'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID,
+                    ]
+                )
+                ->setTemplateVars([
+                    'order' => $order,
+                    'billing' => $order->getBillingAddress(),
+                    'payment_html' => $this->sender->getPaymentHtml($order),
+                    'store' => $order->getStore(),
+                    'formattedShippingAddress' => $this->sender->getFormattedShippingAddress($order),
+                    'formattedBillingAddress' => $this->sender->getFormattedBillingAddress($order),
+                ])
+                ->setFrom($sender)
+                ->addTo($order->getCustomerEmail())
                 ->getTransport();
             $transport->sendMessage();
             $this->inlineTranslation->resume();
